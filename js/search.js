67388148bc1e74df17ae35a06749b4ac674d061d
Kai Gittens <kai.gittens@gmail.com>
2013-05-18 16:03:35 -0400

    whitespace cleanup on js/search.js

--- js/search.js
+++ js/search.js
@@ -2,24 +2,23 @@
 
 define("search", ["jquery","jqui"], function($, jqui) {
   $("#searchbox").autocomplete({
-      source: function( request, response ) {
-        $.ajax({
-          url: "search.json",
-          dataType: "json",
-          data: {term: request.term},
-          success: function(data) {
-            response($.map(data, function(item) {
-              return {
-                label: item.title
-              };
-
-            }));
-          }
-        });
-      },
-      minLength: 2,
-      select: function(event, ui) {
-        console.log(item);
-      }
-    });
+    source: function( request, response ) {
+      $.ajax({
+        url: "search.json",
+        dataType: "json",
+        data: {term: request.term},
+        success: function(data) {
+          response($.map(data, function(item) {
+            return {
+              label: item.title
+            };
+          }));
+        }
+      });
+    },
+    minLength: 2,
+    select: function(event, ui) {
+      console.log(item);
+    }
+  });
 });
