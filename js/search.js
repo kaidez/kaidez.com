2817b3007c204f8ddeca0b53b815107841de0413
Kai Gittens <kai.gittens@gmail.com>
2013-05-24 06:33:43 -0400

    remove tipueContent search dependency from search.js

--- js/search.js
+++ js/search.js
@@ -1,6 +1,9 @@
  
-define("search", ["jquery","jqueryMigrate","tipueContent","tipueset","tipue"], function($, jqueryMigrate, tipueContent, tipueset, tipue) {
+define("search", ["jquery","jqueryMigrate","tipueset","tipue"], function($, jqueryMigrate, tipueset, tipue) {
 
-  $('#tipue_search_input').tipuesearch();
-
+  $('#tipue_search_input').tipuesearch({
+    'mode': 'json',
+    'contentLocation': '/search.json'
+  });
+   
 });
