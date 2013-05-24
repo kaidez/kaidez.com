4b85b57ed1d783633aa426a8c553c036348d2ceb
Kai Gittens <kai.gittens@gmail.com>
2013-05-24 18:22:46 -0400

    set up 'search.js' to show 10 search results and to NOT display URLs

--- js/search.js
+++ js/search.js
@@ -1,6 +1,9 @@
  
 define("search", ["jquery","jqueryMigrate","tipuesetContent","tipueset","tipue"], function($, jqueryMigrate, tipuesetContent, tipueset, tipue) {
 
-     $('#tipue_search_input').tipuesearch();
+    $('#tipue_search_input').tipuesearch({
+      'show': 10,
+      'showURL': false
+    });
    
 });
