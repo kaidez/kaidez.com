1ac0f1bd2a0612593b43b81e9b68241c1dc4257c
kaidez <kai.gittens@gmail.com>
2013-05-24 16:24:23 -0400

    removed extra jQuery ready function from "search.js"

--- js/search.js
+++ js/search.js
@@ -1,8 +1,6 @@
  
 define("search", ["jquery","jqueryMigrate","tipuesetContent","tipueset","tipue"], function($, jqueryMigrate, tipuesetContent, tipueset, tipue) {
 
-  $(document).ready(function() {
      $('#tipue_search_input').tipuesearch();
-});
    
 });
