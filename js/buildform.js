dc7cf69a69f8c8b16e41d2d7c59a6103836b51df
Kai Gittens <kai.gittens@gmail.com>
2013-08-02 05:47:05 -0400

    update comments in js/buildform.js

--- js/buildform.js
+++ js/buildform.js
@@ -1,10 +1,11 @@
 /*
- *   Can 'loadMenu()' be cached? Check: https://gist.github.com/Integralist
+ * Can 'loadMenu()' be cached?
+ * Check: https://gist.github.com/Integralist/1247263
  */
 
 define(function() {
   
-  var loadMenu, cssdisabled, testcss, currstyle; // 4 variables global to this RequireJS module only. 
+  var loadMenu, cssdisabled, testcss, currstyle; // 4 variables global to this RequireJS module only.
 
   /*  
    *  Dynamically create a form that looks like this:
@@ -50,40 +51,50 @@ define(function() {
     loadBox.appendChild(frag);
   }
 
-  /* IS CSS DISABLED ON PAGE-LOAD?
+  /*
+   * IS CSS DISABLED ON PAGE-LOAD?
    *
-   * A very clever way to test if CSS is enabled in the browser. First, create
-   * a Boolean-type variable called 'cssdisabled' and set its value to 'false'.
-   * Then create a <div> with an inline style of 'position:absolute' and place
-   * it directly below the opening <body> tag. Then detect the <div> tag's
-   * position property, but doing this is different between oldIE and everyone
-   * else.
+   * A very clever way to test if CSS is enabled in the browser...
    *
-   * Next, we check to see if the <div> tag has a 'currentStyle' property
-   * attached to it: if it does, we're in IE8 or lower so we use
-   * 'currentStyle' to store the value of the 'position' property inside of
-   * variable called 'currstyle'. Then we check to see if the browser has a
-   * 'getComputedStyle' property attached to it: if it does, we're in another
-   * browser so we use 'getComputedStyle' to store the value of the 'position'
-   * property inside of the 'currstyle' variable.
+   * First, create a Boolean-type variable called 'cssdisabled' and set its 
+   * value to 'false'. Then create a simple <div> tag.  The default value of a 
+   * <div> tag's 'position' property is 'static'...we're going to set/change
+   * it to 'absolute'. After that, we place the <div> directly below the
+   * opening <body> tag.
+   * 
+   * We need to detect the value of the <div> tag's 'position' property, which 
+   * is 'absolute' at this point.  But doing this is different between oldIE 
+   * and other browsers so we need to do it twice.
    *
-   * Now that the <div>'s postion value is stored in 'currstyle' regardless of 
-   * what browser we're in, use a ternary operation to check its value. If
-   * the value is 'static', it means that the browser was unable to detect the <div> tag's absolute positioning we set up earlier with "testcss.style.position = 'absolute'".  Therefore, CSS is disabled in the browser and the value of the 'cssdisabled' variable is 'true'. But if the value is anything else (in this case, 
-   * the 'absolute' value we set earlier), CSS is enabled so the value of our
-   * 'cssdisabled' variable is 'false' (what it was set to initially). Our test is done so we can remove
-   * the <div> tag from the page.
+   * First, we check to see if the <div> tag has a 'currentStyle' property
+   * attached to it: if it does, we're in IE8 or lower so we use 
+   * 'currentStyle' to help us store the value of the 'position' property 
+   * inside of variable called 'currstyle'. Next, we check to see if the 
+   * browser has a 'getComputedStyle' property attached to it: if it does,
+   * we're in another browser so we use 'getComputedStyle' to help us store 
+   * the value of the 'position' property inside of the 'currstyle' variable.
    *
-   * The 'cssdisabled' varibale is still available to our module's namespace so if it's set
-   * to 'false', it means that CSS is enabled in the browser so it's safe to run the loadMenu() method above and display our fancy search box. But 'cssdisabled' is set to
-   * 'true', CSS is NOT enabled so don't run the loadMenu() method and, instead, return
-   * a value of 'false' as it's a best practice in this case.
+   * Now that the <div>'s postion value is stored in 'currstyle' (regardless 
+   * of what browser we're in), we use a ternary operation to find its value. 
+   * If the value is 'static', it means that the browser was unable to detect 
+   * the <div> tag's absolute positioning we set earlier with
+   * 'testcss.style.position:absolute'. Therefore, CSS is disabled in the 
+   * browser and the value of the 'cssdisabled' variable is 'true'. But if the 
+   * value is anything else (in this case, the 'absolute' value we set
+   * earlier), CSS is enabled so the value of our 'cssdisabled' variable is 
+   * 'false' (which is what it was set to at the start initially). At this 
+   * point, we can remove the <div> tag from the page.
    *
-   * We've set thngs up
-   * so that if either any type of CSS is disabled (extenal stylesheets, embedded or inline), our
-   * Google searchbox will show up.  All this means that a searchbox will
-   * appear on our page regardless of whether or not either CSS and/or JS is 
-   * available in the browser.
+   * The 'cssdisabled' variable is still available to our module's namespace so
+   * we can see its value. If we see that the value is set to 'false', it 
+   * means that CSS is enabled in the browser so it's safe to run the
+   * loadMenu() method above and display our fancy searchbox. But if 
+   * 'cssdisabled' is set to true', it means that CSS is NOT enabled. So don't 
+   * run the loadMenu() method and instead, return a value of 'false': a best
+   *  practice in this case.
+   *
+   * We've set thngs up so that if either any type of CSS is disabled (extenal 
+   * stylesheets, embedded or inline), our Google searchbox will show up.
    *  
    *  Attribution for this trick goes to Kethinov who discussed it over at:
    *  http://bit.ly/11iKpZh
