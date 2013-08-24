a1c27e029bee21aaf93d09ed03620c4c379f0105
Kai Gittens <kai.gittens@gmail.com>
2013-08-24 06:35:14 -0400

    find form by ID instead of name (see msg)
    jsPerf test at http://jsperf.com/finding-input-fields proves it's faster to find the form by ID instead of name. change "js/formValidation.js" code to find it by id.

--- js/formValidation.js
+++ js/formValidation.js
@@ -4,15 +4,19 @@
  */
 
 define(function() {
-  
-  var allFields = document.contactForm.getElementsByTagName("input");
-    
+
+  /*
+   * Find the form by ID is faster than finding it by its name.  See the 
+   * jsPerf test at: http://jsperf.com/finding-input-fields.
+   */ 
+  var allFields = document.getElementById("contact").getElementsByTagName("input");
+
     for (key in allFields) {
 
       var theFields = allFields[key];
 
       theFields.onblur = function() {
-        if(this.value === '') {
+        if(this.value === "") {
           console.log("nope");
         }
         
