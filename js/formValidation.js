335fd2471ee7c12243da4872061391a1ff4853e4
Kai Gittens <kai.gittens@gmail.com>
2013-08-28 07:31:22 -0400

    update "js/formValidation.js" module (see msg)
    remove console.log() message that's used for testing. in the blur function, add spanName variable which represents <span> tags that show error messages. also in the blur function, dynamically show error message using a 'getElementById' call.

--- js/formValidation.js
+++ js/formValidation.js
@@ -6,21 +6,22 @@
 define(function() {
 
   /*
-   * Finding the form by ID is faster than finding it by its name.  See the 
-   * jsPerf test at: http://jsperf.com/finding-input-fields.
-   */ 
+   * Finding the form by ID is faster than finding it by its name for the most 
+   * part. See the  jsPerf test at: http://jsperf.com/finding-input-fields.
+   */
   var allFields = document.getElementById("contact").getElementsByTagName("input");
 
     for (key in allFields) {
 
       var theFields = allFields[key];
-
+      
       theFields.onblur = function() {
         if(this.value === "") {
-          console.log("nope");
+          var spanName = this.name + "-error";
+          document.getElementById(spanName).innerHTML = this.name + " is required";
         }
-        
       }
+
     };
 
 });
