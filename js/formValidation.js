23c42b8336074424f4dd28f29ad176b6157553c8
Kai Gittens <kai.gittens@gmail.com>
2013-08-28 16:49:23 -0400

    update error message in "js/formValidation.js" (see msg) -find form by 'forms' instead of 'getElementById'. -have error message generate via 'classList'.

--- js/formValidation.js
+++ js/formValidation.js
@@ -3,13 +3,15 @@
  *
  */
 
+
+
 define(function() {
 
   /*
    * Finding the form by ID is faster than finding it by its name for the most 
    * part. See the  jsPerf test at: http://jsperf.com/finding-input-fields.
    */
-  var allFields = document.getElementById("contact").getElementsByTagName("input");
+  var allFields = document.forms.contact.getElementsByTagName("input");
 
     for (key in allFields) {
 
@@ -20,6 +22,7 @@ define(function() {
           spanNameMsg = document.getElementById(spanName);
         if(this.value === "") {
           spanNameMsg.innerHTML = this.name + " is required";
+          spanNameMsg.classList.toggle("error-fade")
         } else {
           spanNameMsg.innerHTML = "";
         }
