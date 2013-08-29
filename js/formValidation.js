fec9813da71749c2d8061fa5e25fc9e43dfad9c6
Kai Gittens <kai.gittens@gmail.com>
2013-08-29 05:41:59 -0400

    update "js/formValidation.js" (see msg) -add a test for classlist with "Modernizr.addtest" -add comments describing the classlist test

--- js/formValidation.js
+++ js/formValidation.js
@@ -2,10 +2,13 @@
  *
  *
  */
-
-
-
 define(function() {
+
+  /* We need to detect "Modernizr.classlist" but it's a Modernizer non-core 
+   * detect and grunt-modernizr doesn't look for non-core detects. We need to 
+   * manually add it using the Modernizr addTest() API.
+   */
+  Modernizr.addTest("classlist", "classList" in document.documentElement);
 
   /*
    * Finding the form by ID is faster than finding it by its name for the most 
