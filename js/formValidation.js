a36408e579163e4b0d1529eeed06ebadc4838b09
Kai Gittens <kai.gittens@gmail.com>
2013-08-29 14:52:25 -0400

    checking form validation code before refactoring (see msg)
    the code in its current format needs a major refactor. two anonymous functions should probably be resuing code when they aren't. will come back to it sooner than later.

--- js/formValidation.js
+++ js/formValidation.js
@@ -4,7 +4,18 @@
  */
 define(function() {
 
-  /* We need to detect "Modernizr.classlist" but it's a Modernizer non-core 
+  var allFields = document.forms.contact.getElementsByTagName("input"),
+      validationInfo = {
+        "name" : {
+          "required": true
+        },
+        "email" : {
+          "required": true
+        }
+      },
+      storeThisName;
+  /*
+   * We need to detect "Modernizr.classlist" but it's a Modernizer non-core 
    * detect and grunt-modernizr doesn't look for non-core detects. We need to 
    * manually add it using the Modernizr addTest() API.
    */
@@ -14,23 +25,37 @@ define(function() {
    * Finding the form by ID is faster than finding it by its name for the most 
    * part. See the  jsPerf test at: http://jsperf.com/finding-input-fields.
    */
-  var allFields = document.forms.contact.getElementsByTagName("input");
-
-    for (key in allFields) {
-
-      var theFields = allFields[key];
-      
-      theFields.onblur = function() {
-        var spanName = this.name + "-error",
-          spanNameMsg = document.getElementById(spanName);
-        if(this.value === "") {
-          spanNameMsg.innerHTML = this.name + " is required";
-          spanNameMsg.classList.toggle("error-fade")
-        } else {
-          spanNameMsg.innerHTML = "";
-        }
+  
+  
+  document.forms.contact.onsubmit = function() {
+    storeThisName = this.name;
+    var spanName = storeThisName + "-error",
+      spanNameMsg = document.getElementById(spanName);
+    for (key in validationInfo) {
+      var myField = document.getElementById(key);
+      if((validationInfo[key].required) && (myField.value === "")) {
+        spanNameMsg.innerHTML = this.name + " is required";
+        spanNameMsg.classList.toggle("error-fade");
       }
+    }
+  }
 
-    };
+  for (key in allFields) {
+
+    var theFields = allFields[key];
+      
+    theFields.onblur = function() {
+      storeThisName = this.name;
+      var spanName = storeThisName + "-error",
+        spanNameMsg = document.getElementById(spanName);
+      if(this.value === "") {
+        spanNameMsg.innerHTML = this.name + " is required";
+        spanNameMsg.classList.toggle("error-fade");
+      } else {
+        spanNameMsg.innerHTML = "";
+      }
+    }
+
+  };
 
 });
