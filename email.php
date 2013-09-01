cd40327f6c52000ad3f1eeec3e5ebad657842e40
Kai Gittens <kai.gittens@gmail.com>
2013-08-31 20:34:48 -0400

    get email.php working before using isset

--- email.php
+++ email.php
@@ -3,22 +3,44 @@
   $formName = $_POST['name'];
   $formEmail = $_POST['email'];
   $formText = $_POST['text'];
+  $form_errors = false;
 
-  if($_POST) {
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $text = $_POST['text'];
+  if ($formName === '') {
+    echo "<span>Sorry, your name is a required field</span>";
+    $form_errors = true;
+  }
 
+  if (strlen($formName) <= 6) {
+    echo "<span>Sorry, your name must be at least 6 characters</span>";
+    $form_errors = true;
+  }
+
+  if(!filter_var($theEmail, FILTER_VALIDATE_EMAIL)) {
+    echo "E-mail is not valid";
+    $form_errors = true;
+  }
+  else {
+    echo "E-mail is valid";
+  }
+
+  // if there are no form errors, submit the form
+  if ($_POST) {
+
+    $name = $formName;
+    $email = $formEmail;
+    $text = $formText;
     $to = "kai.gittens@gmail.com";
     $subject = "Contact form submitted from kaidez.com";
-    $headers = 'From: ' . $email . "\r\n" .
-    'Reply-To: ' . $email . "\r\n" .
+    $headers = 'From: ' . $formEmail . "\r\n" .
+    'Reply-To: ' . $formEmail . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
+
+    // $cleanUpmessage: a variable that tests to see if the message field is 
+    // clean. i.e., make sure that it doesn't contain HTML characters.
     $cleanUpmessage = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
-    $message = "NAME: " . $name . "\r\n\n" . $cleanUpmessage;
-    
-    //send email
+    $message = "NAME: " . $formName . "\r\n\n" . $cleanUpmessage;
+
     mail($to, $subject, $message, $headers);
   }
 
