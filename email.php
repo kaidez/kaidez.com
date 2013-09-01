b56b825219ba925e38163f48bbf9375f05dee64d
Kai Gittens <kai.gittens@gmail.com>
2013-09-01 07:39:02 -0400

    check in email.php before testing new functionality (see msg) The search functionality powered by email.php works fine but always depends on JavaScript being turned on in the browser. Check in the code on the master branch before switching over to a new branch to create a form that works w/ JS turned off. The branch will be called "no-js-form."

--- email.php
+++ email.php
@@ -1,47 +1,39 @@
 <?php
+  // Email Submit
+  // Note: filter_var() requires PHP >= 5.2.0
 
-  $formName = $_POST['name'];
-  $formEmail = $_POST['email'];
-  $formText = $_POST['text'];
-  $form_errors = false;
+  if ( isset($_POST['email']) && isset($_POST['name']) && isset($_POST['text']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL) ) {
+ 
+    // detect & prevent header injections
+    $test = "/(content-type|bcc:|cc:|to:)/i";
+    foreach ( $_POST as $key => $val ) {
+      if ( preg_match( $test, $val ) ) {
+        exit;
+      }
+    }
 
-  if ($formName === '') {
-    echo "<span>Sorry, your name is a required field</span>";
-    $form_errors = true;
-  }
-
-  if (strlen($formName) <= 6) {
-    echo "<span>Sorry, your name must be at least 6 characters</span>";
-    $form_errors = true;
-  }
-
-  if(!filter_var($theEmail, FILTER_VALIDATE_EMAIL)) {
-    echo "E-mail is not valid";
-    $form_errors = true;
-  }
-  else {
-    echo "E-mail is valid";
-  }
-
-  // if there are no form errors, submit the form
-  if ($_POST) {
-
-    $name = $formName;
-    $email = $formEmail;
-    $text = $formText;
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
     $to = "kai.gittens@gmail.com";
     $subject = "Contact form submitted from kaidez.com";
-    $headers = 'From: ' . $formEmail . "\r\n" .
-    'Reply-To: ' . $formEmail . "\r\n" .
+    $headers = 'From: ' . $email . "\r\n" .
+    'Reply-To: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-
-    // $cleanUpmessage: a variable that tests to see if the message field is 
-    // clean. i.e., make sure that it doesn't contain HTML characters.
-    $cleanUpmessage = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
-    $message = "NAME: " . $formName . "\r\n\n" . $cleanUpmessage;
+    /*
+     * $cleanUpmessage: a variable that tests to see if the message field is 
+     * clean. i.e., make sure that it doesn't contain HTML characters.
+     */
+    $cleanUpMessage = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
+    $message = "NAME: " . $name . "\r\n\n" . $cleanUpMessage;
 
     mail($to, $subject, $message, $headers);
+   
+  } elseif (isset($_POST['name']) == "") {
+    echo "<div>Please enter your name.</div>";
+  } elseif ((strlen($_POST['name']) <= 6) ) {
+    echo "<div>Your name must be a minimum of 6 characters.</div>";
   }
 
 ?>
