bed7c5fa6b34740f1e4de6b6068dad004597c8cc
Kai Gittens <kai.gittens@gmail.com>
2013-09-04 18:14:59 -0400

    check in working contact form before tweaking.

--- email.php
+++ email.php
@@ -1,34 +1,46 @@
 <?php
 
-  $name = $_REQUEST['name'];
-  $email = $_REQUEST['email'];
-  $message = $_REQUEST['message'];
-  $formErrors = $false;
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $message = $_POST['text'];
+  $formErrors = false;
 
   // is "Name" filled out?
   if ($name === '') {
     echo "<div>Please enter your name.</div>";
-    $formErrors = $true;
+    $formErrors = true;
   }
 
   // Does "Message" field have at least 25 characters?
   if (strlen($message) < 25) {
     echo "<div>Your Message must be at least 25 characters.</div>";
-    $formErrors = $true;
+    $formErrors = true;
   }
 
   if ($formErrors === false) {
 
-    $cleanUpEmail = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
 
     $to = "kai.gittens@gmail.com";
     $subject = "Contact form submitted from kaidez.com";
-    $headers = 'From: ' . $cleanUpEmail . "\r\n" .
-    'Reply-To: ' . $cleanUpEmail . "\r\n" .
+    $headers = 'From: ' . $email . "\r\n" .
+    'Reply-To: ' . $email . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
-    $cleanUpMessage = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+    /*
+     * $cleanUpmessage: a variable that tests to see if the message field is 
+     * clean. i.e., make sure that it doesn't contain HTML characters.
+     */
+    $cleanUpMessage = filter_var($_POST['text'], FILTER_SANITIZE_STRING);
     $message = "NAME: " . $name . "\r\n\n" . $cleanUpMessage;
+
+    // $to = "kai.gittens@gmail.com";
+    // $subject = "Contact form submitted from kaidez.com";
+    // $headers = 'From: ' . $cleanUpEmail . "\r\n" .
+    // 'Reply-To: ' . $cleanUpEmail . "\r\n" .
+    // 'X-Mailer: PHP/' . phpversion();
+
+    // $cleanUpMessage = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+    // $message = "NAME: " . $name . "\r\n\n" . $cleanUpMessage;
 
     if(mail($to, $subject, $message, $headers)) {
       echo "<div>Good!!!!!!!!</div>";
