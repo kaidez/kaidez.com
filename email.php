d6f22f5bfda08c53d18a1762e50cc411d6b9ad78
Kai Gittens <kai.gittens@gmail.com>
2013-09-04 06:01:11 -0400

    copy changes in email.php

--- email.php
+++ email.php
@@ -4,12 +4,12 @@
   $email = $_REQUEST['email'];
   $text = $_REQUEST['text'];
 
-  // is name filled out?
+  // is "Name" filled out?
   if ($name === '') {
     echo "<div>Please enter your name.</div>";
   }
 
-  // is name filled out?
+  // Does "Message" field have at least 25 characters?
   if (strlen($text) < 25) {
     echo "<div>Your Message must be at least 25 characters.</div>";
   }
