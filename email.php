1bac260c714927b08b3789d42ae54caf2c255c05
Kai Gittens <kai.gittens@gmail.com>
2013-09-04 06:14:29 -0400

    add commented-out "filter_var"...will use later

--- email.php
+++ email.php
@@ -14,4 +14,12 @@
     echo "<div>Your Message must be at least 25 characters.</div>";
   }
 
+
+/*
+ * $cleanUpmessage: a variable that tests to see if the message field is 
+ * clean. i.e., make sure that it doesn't contain HTML characters.
+ */
+
+// $cleanUpMessage = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+// $message = "NAME: " . $name . "\r\n\n" . $cleanUpMessage;
 ?>
