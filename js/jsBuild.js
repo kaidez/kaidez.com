5f3f6a0ca22b8b215ec8575154c6ec4cffa250c9
Kai Gittens <kai.gittens@gmail.com>
2013-09-10 21:59:38 -0400

    push r.js-related build files for Cary L. code review

---
+++ js/jsBuild.js
@@ -0,0 +1,17 @@
+({
+    baseUrl: ".",
+    paths: {
+      backbone: "libs/backbone-min",
+      enquire: "libs/enquire.min",  // AMD build...not released yet
+      jquery: "libs/jquery",
+      ladda: "libs/ladda.min",
+      prism: "libs/prism",
+      spin: "libs/spin.min",
+      tipue: "libs/tipuesearch.min",
+      tipueset: "libs/tipuesearch_set",
+      tipuesetContent: "libs/tipuesearch_content",
+      underscore: "libs/underscore-min"
+    },
+    name: "search",
+    out: "search-built.js"
+})
