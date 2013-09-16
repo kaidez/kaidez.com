f4d363edb01c45ad460a15e7433e9e31884aac3d
kaidez <kai.gittens@gmail.com>
2013-09-16 19:00:28 -0400

    add 'toJson()' call for testing in Backbone code

--- jsBuildOut/formViews.js
+++ jsBuildOut/formViews.js
@@ -5,13 +5,19 @@
 
 define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
 
-/*
+
 
   var MessageInfo = Backbone.Model.extend({
-    subject: "Contact Form From kaidez.com"
+
+    defaults : {
+      subject: "Contact Form From kaidez.com"
+    }
     
   });
- */
+
+  var msg = new MessageInfo();
+  console.log(msg.toJSON());
+
 
 
 
