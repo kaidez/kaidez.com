67061b53f5247291ecd0dfd672a0712b26f22f90
kaidez <kai.gittens@gmail.com>
2013-09-16 18:51:56 -0400

    whitespace cleanup in BB code

--- jsBuildOut/formViews.js
+++ jsBuildOut/formViews.js
@@ -9,6 +9,7 @@ define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
 
   var MessageInfo = Backbone.Model.extend({
     subject: "Contact Form From kaidez.com"
+    
   });
  */
 
