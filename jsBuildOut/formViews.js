4bb3ecd1e724944b2abd062cfd09dc783c59b38a
kaidez <kai.gittens@gmail.com>
2013-09-16 19:22:35 -0400

    add comments to BB code RE: form validation for future reference

--- jsBuildOut/formViews.js
+++ jsBuildOut/formViews.js
@@ -5,14 +5,15 @@
 
 define(['jquery', 'underscore', 'backbone'], function ($, _, Backbone) {
 
-
+  // can this code be used for form validation? check out the Backbone and
+  // see: http://addyosmani.github.io/backbone-fundamentals/#validation
 
   var MessageInfo = Backbone.Model.extend({
 
     defaults : {
       subject: "Contact Form From kaidez.com"
     }
-    
+
   });
 
   var msg = new MessageInfo();
