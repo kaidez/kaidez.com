6846de1f787e3ca1cc6abdda910467875e30ebbc
Kai Gittens <kai.gittens@gmail.com>
2013-09-27 11:37:51 -0400

    remove old, useless 'jsBuildOut' directory (see msg)
    'jsBuildOut' was renamed 'BuildOutJs,' which is in the project folder,
    so delete the former.  Update/remove refs to these folders in _config.yml.

--- jsBuildOut/prismInit.js
+++
@@ -1,8 +0,0 @@
-/*
- * Run Prism.js as an AMD module (http://prismjs.com/)
- */
-define(["prism"],function(Prism) {
-	// Prism is firing off the JavaScript before the CSS is ready.
-	// Run this method to ensure that snippets gets highlighted after everything /  loads. 
-  Prism.highlightAll();
-});
