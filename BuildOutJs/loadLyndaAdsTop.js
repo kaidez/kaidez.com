6f8b0bf61c4166e3e88103d2f2815c762de5a67f
Kai Gittens <kai.gittens@gmail.com>
2013-09-28 18:08:43 -0400

    create new Jekyll layout for the home page (see msg)
    there's JS code that dynamically loads ads onto the home page so it should only
    run on the home page. it's running sitewide so set things up as follows:
    -create a new Jekyll layout called "_layouts/homePage.html" that adds a class name of "homePage" to the <body> tag.
    -add this layout to "index.html", removing the page's old "default" layout.
    -set the JScode in "BuildOutJs/loadLyndaAdsTop.js" to only run if the page's <body> tag has a class
    name of "homePage" (i.e. index.html).

--- BuildOutJs/loadLyndaAdsTop.js
+++ BuildOutJs/loadLyndaAdsTop.js
@@ -1,113 +1,130 @@
-  /*
-   *  Module: 'loadLyndaAdsTop'
-   *  
-   *  Construct a lynda ad
-   */
+      /*
+       *  Module: 'loadLyndaAdsTop'
+       *  
+       *  Construct a lynda ad
+       */
 
-  define(["enquire"], function(enquire) {
-     
-    // if window.matchMedia is not supported, add matchMedia polyfills. Each file
-    // will load twice, check to see if one is cached as it can be hit-or-miss.
-    Modernizr.load({
-      test: window.matchMedia,
-      nope: [
-        "js/libs/matchMedia.js",
-        "js/libs/matchMedia.addListener.js"
-      ]
-    });
 
-    var adBox = document.getElementById("ads");
+    define(["enquire"], function(enquire) {
 
-       var createLyndaAd = function ( el, link, source, alt ) {
-         var frag = document.createDocumentFragment(),
-          pageElement = document.getElementById( el ),
-          linkTag = document.createElement( "a" ),
-          imageTag = document.createElement( "img" );
+      //   if (document.body('.postPage')) {
+      //   console.log("yup");
+      // } else {
+      //   console.log("nope");
+      // }
+         
+        // if window.matchMedia is not supported, add matchMedia polyfills. Each file
+        // will load twice, check to see if one is cached as it can be hit-or-miss.
+        Modernizr.load({
+          test: window.matchMedia,
+          nope: [
+            "js/libs/matchMedia.js",
+            "js/libs/matchMedia.addListener.js"
+          ]
+        });
 
-         // set attributes for ad link
-          linkTag.setAttribute("href", link);
+        var adBox = document.getElementById("ads");
 
-        // set attributes for ad image
-        imageTag.src = source;
-        imageTag.border = "0";
-        imageTag.width = "125";
-        imageTag.height = "125";
-        imageTag.alt = alt;
+           var createLyndaAd = function ( el, link, source, alt ) {
+             var frag = document.createDocumentFragment(),
+              pageElement = document.getElementById( el ),
+              linkTag = document.createElement( "a" ),
+              imageTag = document.createElement( "img" );
 
-        // Arrange elements
-        linkTag.appendChild(imageTag);
+             // set attributes for ad link
+              linkTag.setAttribute("href", link);
 
-        // Load arranged elements into document fragment
-        frag.appendChild(linkTag);
-        
-        // Load document fragment into '#aside-ad-section'
-        pageElement.appendChild(frag);
+            // set attributes for ad image
+            imageTag.src = source;
+            imageTag.border = "0";
+            imageTag.width = "125";
+            imageTag.height = "125";
+            imageTag.alt = alt;
 
-     };
-     
-    var adCodes = {
-        "css" : {
-              "pageEl" : "adSpotOne",
-              "link"      : "http://www.lynda.com/CSS-training-tutorials/447-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=637&utm_campaign=CD2146&bid=637&aid=CD2146",
-              "source" : "http://lynda.directtrack.com/42/2146/637/",
-              "alt"       : "Learn CSS"
+            // Arrange elements
+            linkTag.appendChild(imageTag);
+
+            // Load arranged elements into document fragment
+            frag.appendChild(linkTag);
+            
+            // Load document fragment into '#aside-ad-section'
+            pageElement.appendChild(frag);
+
+         };
+         
+        var adCodes = {
+            "css" : {
+                  "pageEl" : "adSpotOne",
+                  "link"      : "http://www.lynda.com/CSS-training-tutorials/447-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=637&utm_campaign=CD2146&bid=637&aid=CD2146",
+                  "source" : "http://lynda.directtrack.com/42/2146/637/",
+                  "alt"       : "Learn CSS"
+              },
+              "javascript" : {
+                  "pageEl" : "adSpotTwo",
+                  "link"      : "http://www.lynda.com/JavaScript-training-tutorials/244-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=639&utm_campaign=CD2146&bid=639&aid=CD2146",
+                  "source" : "http://lynda.directtrack.com/42/2146/639/",
+                  "alt"       : "Learn Javascript"
+              }
+          };
+
+        // enquire.js code:  manages when the lynda.com add displays
+        enquire.register("only screen and (min-width: 729px)", {
+
+          /*
+           * The 'setup' method below runs 'showAd()'. This 'deferSetup' 
+           * method makes sure that 'setup' only runs when it's media query is
+           * first matched. In other words, only run it when it's on a device that
+           * has 729px width setting above, tablets in this website's case.
+           * Why run it on smaller devices when we don't want it...ya know?!?! 
+           */
+          deferSetup : true,
+
+          /*
+           * Stores the 'showAd' method in a callback so enquire only runs it 
+           * once. Our code is optimized now.
+           */
+          setup : function() {
+            
+            if (document.body.className == "homePage") {
+              for ( key in adCodes ) {
+                if ( adCodes.hasOwnProperty(key) ) {
+                  var e = adCodes[key].pageEl,
+                      l  = adCodes[key].link,
+                      s = adCodes[key].source,
+                      a = adCodes[key].alt;
+                  createLyndaAd(e, l, s, a);
+                }
+             }  
+           }
           },
-          "javascript" : {
-              "pageEl" : "adSpotTwo",
-              "link"      : "http://www.lynda.com/JavaScript-training-tutorials/244-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=639&utm_campaign=CD2146&bid=639&aid=CD2146",
-              "source" : "http://lynda.directtrack.com/42/2146/639/",
-              "alt"       : "Learn Javascript"
+
+          /*
+           * If the we have a 'match' in our media query (i.e. if the site's being
+           * looked at on a device that's at least 729px wide), display the box that 
+           * contains the Lynda ad. Also, since 'match()' as run, this means that 
+           * 'showAd()' has run and has been stored it in a callback for 
+           * future use.
+           */
+          match : function() {
+            if (document.body.className == "homePage") {
+              ads.style.display="inline-block";
+            } else {
+              return false;
+            }
+          },
+
+          /*
+           * If our media query is 'unmatched' (i.e. if the site's being
+           * looked at on a device that's  568px wide or less), don't display the 
+           * box that contains the Lynda ad.
+           */
+          unmatch : function() {
+            if (document.body.className == "homePage") {
+              ads.style.display="none";
+          } else {
+            return false;
           }
-      };
 
-    // enquire.js code:  manages when the lynda.com add displays
-    enquire.register("only screen and (min-width: 729px)", {
+        }, true);
 
-      /*
-       * The 'setup' method below runs 'showAd()'. This 'deferSetup' 
-       * method makes sure that 'setup' only runs when it's media query is
-       * first matched. In other words, only run it when it's on a device that
-       * has 729px width setting above, tablets in this website's case.
-       * Why run it on smaller devices when we don't want it...ya know?!?! 
-       */
-      deferSetup : true,
-
-      /*
-       * Stores the 'showAd' method in a callback so enquire only runs it 
-       * once. Our code is optimized now.
-       */
-      setup : function() {
-        for ( key in adCodes ) {
-          if ( adCodes.hasOwnProperty(key) ) {
-            var e = adCodes[key].pageEl,
-                  l  = adCodes[key].link,
-                  s = adCodes[key].source,
-                  a = adCodes[key].alt;
-            createLyndaAd(e, l, s, a);
-          }
-       }  
-      },
-
-      /*
-       * If the we have a 'match' in our media query (i.e. if the site's being
-       * looked at on a device that's at least 729px wide), display the box that 
-       * contains the Lynda ad. Also, since 'match()' as run, this means that 
-       * 'showAd()' has run and has been stored it in a callback for 
-       * future use.
-       */
-      match : function() {
-        ads.style.display="inline-block";
-      },
-
-      /*
-       * If our media query is 'unmatched' (i.e. if the site's being
-       * looked at on a device that's  568px wide or less), don't display the 
-       * box that contains the Lynda ad.
-       */
-      unmatch : function() {
-        ads.style.display="none";
-      }
-
-    }, true);
-
-  });
+      });
