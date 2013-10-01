2967c80b59ebcb818f21d2608187de6ef2e86b75
Kai Gittens <kai.gittens@gmail.com>
2013-09-30 22:06:43 -0400

    page content container & breadcrumb styling changes

--- requireBuildOut/classListHelper.js
+++ requireBuildOut/classListHelper.js
@@ -3,8 +3,8 @@
  *
  * In order to safely use 'toggle()', we need to detect
  * "Modernizr.classlist". But it's a Modernizr non-core detect and the 
- * 'grunt-modernizr' doesn't look for non-core detects when it builds out our 
- * Modernizr custom build. We need to manually add it using the Modernizr addTest() API.
+ * 'grunt-modernizr' doesn't look for non-core detects when it builds * out our Modernizr custom build. We need to manually add it using 
+ * the Modernizr addTest() API.
  */
 
 define(function() {
