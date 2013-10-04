d8f076536f4263d8b82457dae50f10f17352a1b1
Kai Gittens <kai.gittens@gmail.com>
2013-10-04 06:50:43 -0400

    update the file array for the "grunt-modernizr" task

--- Gruntfile.js
+++ Gruntfile.js
@@ -190,7 +190,7 @@ module.exports = function(grunt) {
 
       // When parseFiles = true, this task will crawl all *.js, *.css, *.scss files.
       // You can override this by defining a "files" array below.
-      "files" : ["css/styles.min.css","jsBuild/*.js","jsBuild/**/*.js"],
+      "files" : ["css/styles.min.css","requireBuildOut/*.js","requireBuildOut/**/*.js"],
 
       // When parseFiles = true, matchCommunityTests = true will attempt to
       // match user-contributed tests.
