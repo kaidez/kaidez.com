63d9945587b7b4b2f1d5240051285287bdc15f63
kaidez <kai.gittens@gmail.com>
2013-11-14 09:14:41 -0500

    add code that changes no-js to js for tipue demo...add comments

--- samples/jekyll-search/js/scripts.js
+++ samples/jekyll-search/js/scripts.js
@@ -1,7 +1,16 @@
 (function(){
 
+    // The Tipue-powered code that returns search results to
+    // "search.html".
     $(function() {
       $('#tipue_search_input').tipuesearch();
     });
+    
+    // If JavaScript is enabled, this code will change the "no-js" 
+    // class on the opening <html> element to "js". This code is stolen
+    // from Modernizr, so if Modernizr is already on your web page,
+    // don't use this part od the code.
+    var docElement = document.documentElement;
+    docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') + ('js');
 
 })();
