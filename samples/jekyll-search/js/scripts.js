9670d6609ad543a57d10bfeeda4c525cc8833316
kaidez <kai.gittens@gmail.com>
2013-11-14 13:02:18 -0500

    update Tipue search functionality. put JS-enabled detect in jsDetect.js

--- samples/jekyll-search/js/scripts.js
+++ samples/jekyll-search/js/scripts.js
@@ -6,11 +6,4 @@
       $('#tipue_search_input').tipuesearch();
     });
     
-    // If JavaScript is enabled, this code will change the "no-js" 
-    // class on the opening <html> element to "js". This code is stolen
-    // from Modernizr so if Modernizr is already on your web page,
-    // don't use this part of the code.
-    var docElement = document.documentElement;
-    docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') + ('js');
-
 })();
