dad861adb2146a58035d5d91b78a54a257958331
kaidez <kai.gittens@gmail.com>
2013-11-14 09:59:55 -0500

    update Tipue demno code: WORKING

--- samples/jekyll-search/js/scripts.js
+++ samples/jekyll-search/js/scripts.js
@@ -8,7 +8,7 @@
     
     // If JavaScript is enabled, this code will change the "no-js" 
     // class on the opening <html> element to "js". This code is stolen
-    // from Modernizr, so if Modernizr is already on your web page,
+    // from Modernizr so if Modernizr is already on your web page,
     // don't use this part of the code.
     var docElement = document.documentElement;
     docElement.className = docElement.className.replace(/(^|\s)no-js(\s|$)/, '$1$2') + ('js');
