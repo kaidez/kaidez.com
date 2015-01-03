d89953dec44ef8d6a8da0cdf37d5ba4f96e1c527
kaidez <kai.gittens@gmail.com>
2015-01-03 17:06:48 -0500

    update some scripts

--- samples/ajax-tutorial-samples/sample01/scripts.js
+++ samples/ajax-tutorial-samples/sample01/scripts.js
@@ -2,10 +2,8 @@
 var xhr;
 if (window.XMLHttpRequest) { // Browsers other than IE 6 and lower
   xhr = new XMLHttpRequest();
-  alert("Supports newer XHR implementations");
 } else {
   if (window.ActiveXObject) { // For IE 6 and lower
     xhr = new ActiveXObject("Microsoft.XMLHTTP");
-    alert("Supports older XHR implementations");
   }
 }
--- samples/ajax-tutorial-samples/sample02/scripts.js
+++ samples/ajax-tutorial-samples/sample02/scripts.js
@@ -4,18 +4,14 @@ function getXHR() {
   var xhr;
   if (window.XMLHttpRequest) {
     xhr = new XMLHttpRequest();
-    alert("Supports newer XHR implementations");
   } else {
     try {
       xhr = new ActiveXObject("Msxml2.XMLHTTP");
-      alert("Supports one version of ActiveX");
     } catch (e) {
       try {
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
-        alert("Supports another version of ActiveX");
       } catch (e) {
         xhr = false;
-        alert("Sorry...this browser doesn't support XMLHttpRequest");
       }
     }
   }
