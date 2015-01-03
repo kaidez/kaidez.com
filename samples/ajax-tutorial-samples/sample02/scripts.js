0001da55e4c7d5a5627e5bbc2f95b25e6fd1f5cc
kaidez <kai.gittens@gmail.com>
2015-01-03 17:38:38 -0500

    update sample02 javascript

--- samples/ajax-tutorial-samples/sample02/scripts.js
+++ samples/ajax-tutorial-samples/sample02/scripts.js
@@ -2,16 +2,18 @@
 // More robust detecting of ActiveX implementations
 function getXHR() {
   var xhr;
-  if (window.XMLHttpRequest) {
+  if (window.XMLHttpRequest) { // Browsers other than IE 6 and lower
     xhr = new XMLHttpRequest();
   } else {
-    try {
+    try { // Browsers with one type of ActiveXObject build
       xhr = new ActiveXObject("Msxml2.XMLHTTP");
     } catch (e) {
-      try {
+      try { // Browsers with another type of ActiveXObject build
         xhr = new ActiveXObject("Microsoft.XMLHTTP");
       } catch (e) {
+        // Browsers that don't support either XMLHttpRequest or ActiveXObject
         xhr = false;
+        alert("Sorry...XMLHttpRequest is not supported.")
       }
     }
   }
