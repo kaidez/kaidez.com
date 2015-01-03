a62be5cbb1b64ff855812c8bce36a7ebaa64d0e4
kaidez <kai.gittens@gmail.com>
2015-01-03 09:20:16 -0500

    change alerts, add whitespace, update comments

--- samples/ajax-tutorial-samples/sample02/scripts.js
+++ samples/ajax-tutorial-samples/sample02/scripts.js
@@ -1,23 +1,23 @@
 // Feature-detect XMLHttpRequest implementation
 // More robust detecting of ActiveX implementations
-(function(){
+function getXHR() {
   var xhr;
-    if (window.XMLHttpRequest) {
-      xhr = new XMLHttpRequest();
-      alert("Supports newer XHR implementations");
-    } else {
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+    alert("Supports newer XHR implementations");
+  } else {
+    try {
+      xhr = new ActiveXObject("Msxml2.XMLHTTP");
+      alert("Supports one version of ActiveX");
+    } catch (e) {
       try {
-        xhr = new ActiveXObject("Msxml2.XMLHTTP");
-        alert("Supports one version of ActiveX");
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        alert("Supports another version of ActiveX");
       } catch (e) {
-        try {
-          xhr = new ActiveXObject("Microsoft.XMLHTTP");
-          alert("Supports another version of ActiveX");
-        } catch (e) {
-          xhr = false;
-          alert("Sorry...xhr is not supported");
-        }
+        xhr = false;
+        alert("Sorry...this browser doesn't support XMLHttpRequest");
       }
     }
+  }
   return xhr;
-})();
+}
