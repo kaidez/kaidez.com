a1c92cab41add21f0605c4e33336754e0d9de170
kaidez <kai.gittens@gmail.com>
2015-01-04 08:46:46 -0500

    remove alert

--- samples/ajax-tutorial-samples/sample02/scripts.js
+++ samples/ajax-tutorial-samples/sample02/scripts.js
@@ -13,7 +13,6 @@ function getXHR() {
       } catch (e) {
         // Browsers that don't support either XMLHttpRequest or ActiveXObject
         xhr = false;
-        alert("Sorry...XMLHttpRequest is not supported.")
       }
     }
   }
