60b2ace654fd348901bd850077b2441f87322a5b
kaidez <kai.gittens@gmail.com>
2015-01-05 12:18:13 -0500

    start "sample04/" directory

---
+++ samples/ajax-tutorial-samples/sample04/scripts.js
@@ -0,0 +1,36 @@
+// Feature-detect XMLHttpRequest implementation
+// More robust detecting of ActiveX implementations
+function getXHR() {
+  var xhr;
+  if (window.XMLHttpRequest) {
+    xhr = new XMLHttpRequest();
+  } else {
+    try {
+      xhr = new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {
+      try {
+        xhr = new ActiveXObject("Microsoft.XMLHTTP");
+      } catch (e) {
+        xhr = false;
+      }
+    }
+  }
+  return xhr;
+}
+
+var getArticleInfo = new getXHR();
+
+getArticleInfo.onreadystatechange = loadText;
+getArticleInfo.open("GET", "articleName.txt");
+getArticleInfo.send();
+
+function loadText() {
+  var text = document.getElementById("textTarget");
+    if (getArticleInfo.readyState === 4) {
+      if (getArticleInfo.status === 200) {
+        text.innerHTML = getArticleInfo.responseText;
+      } else {
+        console.log('There was a problem with the request.');
+      }
+    }
+};
