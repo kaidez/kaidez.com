9f25062df025419591b0b196ad80987bef24c504
kaidez <kai.gittens@gmail.com>
2015-01-06 13:16:34 -0500

    test & add time stamps for the AJAX click querySelectorAll functionality

--- samples/ajax-tutorial-samples/sample10/scripts.js
+++ samples/ajax-tutorial-samples/sample10/scripts.js
@@ -16,17 +16,30 @@ function loadFile(file) {
   }
 }
 
+// Time stamps
+// after the loop starts: 0.066ms
+// scripts.js:34 after the loop starts: 0.040ms
+// scripts.js:34 after the loop starts: 0.066ms
+// scripts.js:34 after the loop starts: 0.020ms
+// scripts.js:36 the whole loop: 1.120ms
+// scripts.js:32 after click: 0.478ms
+// scripts.js:32 after click: 0.529ms
+
 // The first click takes over a asecond to make the AJAX call
 // The clicks run the code really fast after that
 var buttons = document.querySelectorAll(".btn");
-console.time("run");
-  for (key in buttons) {
-    var theButtons = buttons[key];
 
-    // If a form field is blurred, validate it
-    theButtons.onclick = function() {
-      loadFile(this.dataset.file);
-    }
+console.time("the whole loop");
+for (key in buttons) {
+  console.time("after the loop starts");
+  var theButtons = buttons[key];
 
+  // If a form field is blurred, validate it
+  theButtons.onclick = function() {
+    console.time("after click");
+    loadFile(this.dataset.file);
+    console.timeEnd("after click");
+  }
+console.timeEnd("after the loop starts");
 }
-console.timeEnd("run");
+console.timeEnd("the whole loop");
