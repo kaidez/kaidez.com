10b69db956e7b6b2326312b67e3a4699ca4742fe
kaidez <kai.gittens@gmail.com>
2015-01-28 05:52:55 -0500

    update sample code

--- samples/ajax-tutorial-samples/sample01/scripts.js
+++ samples/ajax-tutorial-samples/sample01/scripts.js
@@ -1,24 +1,4 @@
-// Feature-detect XMLHttpRequest implementation
-// More robust detecting of ActiveX implementations
-function getXHR() {
-  var xhr;
-  if (window.XMLHttpRequest) {
-    xhr = new XMLHttpRequest();
-  } else {
-    try {
-      xhr = new ActiveXObject("Msxml2.XMLHTTP");
-    } catch (e) {
-      try {
-        xhr = new ActiveXObject("Microsoft.XMLHTTP");
-      } catch (e) {
-        xhr = false;
-      }
-    }
-  }
-  return xhr;
-}
-
-var getArticleInfo = new getXHR();
+var getArticleInfo = new XMLHttpRequest();
 
 getArticleInfo.onreadystatechange = loadText;
 getArticleInfo.open("GET", "articleName.txt");
