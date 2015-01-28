dceb7bb970586b7923de6837167574f65c3acc5d
kaidez <kai.gittens@gmail.com>
2015-01-27 20:45:38 -0500

    remove useless sample folders

--- samples/ajax-tutorial-samples/sample01/scripts.js
+++
@@ -1,9 +0,0 @@
-// Feature-detect XMLHttpRequest implementation
-var xhr;
-if (window.XMLHttpRequest) { // Browsers other than IE 6 and lower
-  xhr = new XMLHttpRequest();
-} else {
-  if (window.ActiveXObject) { // For IE 6 and lower
-    xhr = new ActiveXObject("Microsoft.XMLHTTP");
-  }
-}
