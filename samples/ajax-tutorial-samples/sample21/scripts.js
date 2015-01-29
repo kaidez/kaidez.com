7361225d906d112f7081c07ceb2f402ee238e611
kaidez <kai.gittens@gmail.com>
2015-01-29 14:37:35 -0500

    update sample21 code

--- samples/ajax-tutorial-samples/sample21/scripts.js
+++ samples/ajax-tutorial-samples/sample21/scripts.js
@@ -1,3 +1,4 @@
+// Try & load "article.html" into "<div id="textTarget">"
 $.get("article.html")
   .done(function(data) {
     $("#textTarget").html(data);
@@ -6,5 +7,17 @@ $.get("article.html")
     $("#textTarget").html("The file didn't load!");
   })
   .always(function(){
-    console.log("The file either did or didn'r load!");
+    console.log("The 'article.html' file either did or didn't load!");
+  });
+
+// Try & load "article02.html" into "<div id="textTarget02">"
+$.get("article02.html")
+  .done(function(data) {
+    $("#textTarget02").html(data);
+  })
+  .fail(function() {
+    $("#textTarget02").html("The 'article02.html' file didn't load!");
+  })
+  .always(function(){
+    console.log("The 'article02.html' file either did or didn't load!");
   });
