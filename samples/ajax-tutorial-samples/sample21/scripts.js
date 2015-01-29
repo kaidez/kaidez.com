c7341d729a44d4ae81316b82e1ac7bcaa453328c
kaidez <kai.gittens@gmail.com>
2015-01-29 06:30:34 -0500

    update sample21 code

--- samples/ajax-tutorial-samples/sample21/scripts.js
+++ samples/ajax-tutorial-samples/sample21/scripts.js
@@ -1,3 +1,10 @@
-$.get("article.html").always(function(data) {
+$.get("article.html")
+  .done(function(data) {
     $("#textTarget").html(data);
-});
+  })
+  .fail(function() {
+    $("#textTarget").html("The file didn't load!");
+  })
+  .always(function(){
+    console.log("The file either did or didn'r load!");
+  });
