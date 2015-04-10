ff69744a504565903eab18b35f75243b0aeff1a1
kaidez <kai.gittens@gmail.com>
2015-04-10 08:37:44 -0400

    update styles related to the aside

--- kaidez-swiss/wp-content/themes/kaidez-swiss/sidebar.php
+++ kaidez-swiss/wp-content/themes/kaidez-swiss/sidebar.php
@@ -5,11 +5,37 @@
  * @package kaidez-swiss
  */
 
-if ( ! is_active_sidebar( 'sidebar-1' ) ) {
-	return;
-}
-?>
 
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</div><!-- #secondary -->
+?>
+<aside id="aside-id" class="aside">
+  <h3 class="aside-header">Other Site Information</h3>
+  <div class="row aside-inner">
+  	<section class="col-md-4">
+  	  <h4 class="aside-section-header">social</h4>
+  	  <ul>
+  	  	<li>
+  	  	  <a href="http://facebook.com/kaidezblog" alt="follow kaidez on facebook">facebook</a>
+  	  	</li>
+  	  	<li>
+  	  		<a href="http://twitter.com/kaidez" alt="follow kaidez on twitter">twitter</a>
+  	  	</li>
+  	  	 <li>
+  	  		<a href="http://youtube.com/c/kaidezblog" alt="follow kaidez on YouTube">youtube</a>
+  	  	</li>
+  	  	<li>
+  	  		<a href="http://github.com/kaidez" alt="follow kaidez on YouTube">github</a>
+  	  	</li>
+  	  	<li>
+  	  		<a href="https://plus.google.com/+KaiGittens" alt="follow kaidez on Google +">google+</a>
+  	  	</li>
+  	  </ul>
+    </section>
+    <section class="col-md-4">
+      <h4 class="aside-section-header">categories</h4>
+    </section>
+    <section class="col-md-4">
+      <h4 class="aside-section-header">site navigation</h4>
+    </section>
+  </div>	
+</aside><!-- #aside-id -->
+
