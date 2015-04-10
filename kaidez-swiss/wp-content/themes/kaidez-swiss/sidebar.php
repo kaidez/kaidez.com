b1575ba828a1ab1563d46a1fee7267c506aa4e94
kaidez <kai.gittens@gmail.com>
2015-04-10 17:37:10 -0400

    add "aside-link" class to all <aside> links

--- kaidez-swiss/wp-content/themes/kaidez-swiss/sidebar.php
+++ kaidez-swiss/wp-content/themes/kaidez-swiss/sidebar.php
@@ -14,19 +14,19 @@
   	  <h4 class="aside-section-header">social</h4>
   	  <ul class="aside-list">
   	  	<li class="aside-list-item">
-  	  	  <a href="http://facebook.com/kaidezblog" alt="follow kaidez on facebook"><i class="fa fa-facebook fa-lg"></i> facebook</a>
+  	  	  <a href="http://facebook.com/kaidezblog" class="aside-link" alt="follow kaidez on facebook"><i class="fa fa-facebook fa-lg"></i> facebook</a>
   	  	</li>
   	  	<li class="aside-list-item">
-  	  		<a href="http://twitter.com/kaidez" alt="follow kaidez on twitter"><i class="fa fa-twitter fa-lg"></i> twitter</a>
+  	  		<a href="http://twitter.com/kaidez" class="aside-link" alt="follow kaidez on twitter"><i class="fa fa-twitter fa-lg"></i> twitter</a>
   	  	</li>
   	  	 <li class="aside-list-item">
-  	  		<i class="fa fa-youtube fa-lg"></i><a href="http://youtube.com/c/kaidezblog" alt="follow kaidez on YouTube">youtube</a>
+  	  		<i class="fa fa-youtube fa-lg"></i><a href="http://youtube.com/c/kaidezblog" class="aside-link" alt="follow kaidez on YouTube">youtube</a>
   	  	</li>
   	  	<li class="aside-list-item">
-  	  		<a href="http://github.com/kaidez" alt="follow kaidez on YouTube"><i class="fa fa-github fa-lg"></i> github</a>
+  	  		<a href="http://github.com/kaidez" class="aside-link" alt="follow kaidez on YouTube"><i class="fa fa-github fa-lg"></i> github</a>
   	  	</li>
   	  	<li class="aside-list-item">
-  	  		<a href="https://plus.google.com/+KaiGittens" alt="follow kaidez on Google +"><i class="fa fa-google-plus fa-lg"></i> google+</a>
+  	  		<a href="https://plus.google.com/+KaiGittens" class="aside-link" alt="follow kaidez on Google +"><i class="fa fa-google-plus fa-lg"></i> google+</a>
   	  	</li>
   	  </ul>
     </section>
