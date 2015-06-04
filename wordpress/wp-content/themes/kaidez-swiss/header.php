663a0c4b18fbf232294066e16118d5c2b2582c11
Kai Gittens <kai.gittens@gmail.com>
2015-06-03 22:02:15 -0400

    add search form back

--- wordpress/wp-content/themes/kaidez-swiss/header.php
+++ wordpress/wp-content/themes/kaidez-swiss/header.php
@@ -32,6 +32,13 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'kaidez-swiss' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+
+		<form method="get" id="searchform" class="searchform-class" action="<?php bloginfo('home'); ?>/">
+      <div class="blog-search">
+        <input id="s" class="header-search-field" type="text" size="18" value="<?php echo wp_specialchars($s, 1); ?>" name="s" id="s" class="header-search-field" />
+        <button class="btn btn-default header-search-submit footer-button" type="submit" id="searchsubmit" />Submit</button>
+      </div>
+    </form>
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
