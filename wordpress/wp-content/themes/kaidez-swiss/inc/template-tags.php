3f406021e5b578f263addda999bc1815441a58b4
Kai Gittens <kai.gittens@gmail.com>
2015-06-07 11:19:16 -0400

    remove PHP code that displays "time-post-updated" info

--- wordpress/wp-content/themes/kaidez-swiss/inc/template-tags.php
+++ wordpress/wp-content/themes/kaidez-swiss/inc/template-tags.php
@@ -70,9 +70,9 @@ if ( ! function_exists( 'kaidez_swiss_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function kaidez_swiss_posted_on() {
-	$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
-		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 	}
 
 	$time_string = sprintf( $time_string,
