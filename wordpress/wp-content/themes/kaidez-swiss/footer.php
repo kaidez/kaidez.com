de5191390e424b1f83cedefa38955d5fa99ff7fd
Kai Gittens <kai.gittens@gmail.com>
2015-06-08 06:45:54 -0400

    update footer.php (see msg) -re-indent copy -style footer links

--- wordpress/wp-content/themes/kaidez-swiss/footer.php
+++ wordpress/wp-content/themes/kaidez-swiss/footer.php
@@ -8,78 +8,85 @@
  */
 
 ?>
-    <aside id="aside-id" class="aside">
-      <h3 class="aside-header">Other Site Information</h3>
-      <div class="row aside-inner">
-        <section class="col-md-4">
-          <h4 class="aside-section-header">social</h4>
-          <ul class="aside-list">
-            <li class="aside-list-item">
-              <a href="http://facebook.com/kaidezblog" class="aside-link" alt="follow kaidez on facebook"><i class="fa fa-facebook fa-lg"></i> facebook</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="http://twitter.com/kaidez" class="aside-link" alt="follow kaidez on twitter"><i class="fa fa-twitter fa-lg"></i> twitter</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="http://youtube.com/c/kaidezblog" class="aside-link" alt="follow kaidez on YouTube"><i class="fa fa-youtube fa-lg"></i> youtube</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="http://github.com/kaidez" class="aside-link" alt="follow kaidez on YouTube"><i class="fa fa-github fa-lg"></i> github</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="https://plus.google.com/+KaiGittens" class="aside-link" alt="follow kaidez on Google +"><i class="fa fa-google-plus fa-lg"></i> google+</a>
-            </li>
-          </ul>
-        </section>
-        <section class="col-md-4">
-          <h4 class="aside-section-header">categories</h4>
-          <ul class="aside-list">
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read tutorials on the kaidez blog">tutorials</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read code tips on the kaidez blog">code tips</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="/personal/" class="aside-link" alt="read personal posts on the kaidez blog">personal</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read old reviews on the kaidez blog">reviews</a>
-            </li>
-          </ul>
-        </section>
-        <section class="col-md-4">
-          <h4 class="aside-section-header">site navigation</h4>
-          <ul class="aside-list">
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read the kaidez blog">blog</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read articles written by blog">articles</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read the AJAX beginner's tutorial written by kaidez">ajax tutorial</a>
-            </li>
-            <li class="aside-list-item">
-              <a href="#" class="aside-link" alt="read the lynda.com courses developed by kaidez">kaidez on lynda</a>
-            </li>
-          </ul>
-        </section>
-      </div>  
-    </aside> <!-- #aside-id -->
+<aside id="aside-id" class="aside">
+  <h3 class="aside-header">Other Site Information</h3>
+  <div class="row aside-inner">
 
-    <footer id="colophon" class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
-      &copy; 2008-15 Kai Gittens
-    </footer><!-- #colophon -->
-    </div><!-- #page -->
+    <section id="footer-social-links" class="col-md-4">
+      <h4 class="aside-section-header">social</h4>
+      <ul class="aside-list">
+        <li class="aside-list-item">
+          <a href="http://facebook.com/kaidezblog" class="aside-link" alt="follow kaidez on facebook"><i class="fa fa-facebook fa-lg"></i> facebook</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="http://twitter.com/kaidez" class="aside-link" alt="follow kaidez on twitter"><i class="fa fa-twitter fa-lg"></i> twitter</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="http://youtube.com/c/kaidezblog" class="aside-link" alt="follow kaidez on YouTube"><i class="fa fa-youtube fa-lg"></i> youtube</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="http://github.com/kaidez" class="aside-link" alt="follow kaidez on YouTube"><i class="fa fa-github fa-lg"></i> github</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="https://plus.google.com/+KaiGittens" class="aside-link" alt="follow kaidez on Google +"><i class="fa fa-google-plus fa-lg"></i> google+</a>
+        </li>
+      </ul>
+    </section> <!-- #footer-social-links -->
 
-    <?php wp_footer(); ?>
-  </div><!-- #content -->
-  <script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
-  <script>
-    $("#mobile-menu-button").click(function(){
-      $("#site-navigation").addClass("kai");
-    });
-  </script>
+    <section id="footer-category-links" class="col-md-4">
+      <h4 class="aside-section-header">categories</h4>
+      <ul class="aside-list">
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read tutorials on the kaidez blog">tutorials</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read code tips on the kaidez blog">code tips</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="/personal/" class="aside-link" alt="read personal posts on the kaidez blog">personal</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read old reviews on the kaidez blog">reviews</a>
+        </li>
+      </ul>
+    </section> <!-- #footer-category-links -->
+
+    <section id="footer-navigation" class="col-md-4">
+      <h4 class="aside-section-header">site navigation</h4>
+      <ul class="aside-list">
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read the kaidez blog">blog</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read articles written by blog">articles</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read the AJAX beginner's tutorial written by kaidez">ajax tutorial</a>
+        </li>
+        <li class="aside-list-item">
+          <a href="#" class="aside-link" alt="read the lynda.com courses developed by kaidez">kaidez on lynda</a>
+        </li>
+      </ul>
+    </section> <!-- #footer-navigation -->
+  </div>  
+</aside> <!-- #aside-id -->
+
+<footer id="colophon" class="site-footer" role="contentinfo" itemscope="itemscope" itemtype="http://schema.org/WPFooter">
+  <p class="footer-text">&copy; 2008-15 Kai Gittens</p>
+  <p class="footer-text">
+    <a href="" class="footer-links">site map</a>
+    <a href="" class="footer-links">affiliate disclaimer</a>
+  </p>
+</footer> <!-- #colophon -->
+</div> <!-- #page -->
+
+<?php wp_footer(); ?>
+</div> <!-- #content -->
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script>
+  $("#mobile-menu-button").click(function(){
+    $("#site-navigation").addClass("kai");
+  });
+</script>
 </body>
 </html>
