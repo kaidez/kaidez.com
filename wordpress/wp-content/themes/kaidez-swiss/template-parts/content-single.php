9332581a4c18813ccf5379ca86fcb4d641403ae8
Kai Gittens <kai.gittens@gmail.com>
2015-06-21 11:09:08 -0400

    add structured data to single post page

--- wordpress/wp-content/themes/kaidez-swiss/template-parts/content-single.php
+++ wordpress/wp-content/themes/kaidez-swiss/template-parts/content-single.php
@@ -7,9 +7,9 @@
 
 ?>
 
-  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> itemscope="itemscope" itemtype="http://schema.org/BlogPosting" itemprop="blogPost">
     <header class="entry-header">
-      <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+      <?php the_title( '<h1 class="entry-title" itemprop="headline">', '</h1>' ); ?>
   
       <div class="entry-meta">
         <?php kaidez_swiss_posted_on(); ?>
