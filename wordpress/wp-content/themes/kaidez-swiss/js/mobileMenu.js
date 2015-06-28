e48b1514109064c4bc9046a03a9a8d224520b8fd
Kai Gittens <kai.gittens@gmail.com>
2015-06-28 06:39:56 -0400

    update mobileMenu.js

--- wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
+++ wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
@@ -3,6 +3,30 @@ $( "#mobile-menu-button" ).click( function() {
   var isMobileMenu = $( "body" ).hasClass( "show-mobile-menu" );
 
   if( !isMobileMenu ){
+    $( "#masthead" ).addClass( "show-menu" ).removeClass( "hide-menu" );
+    $( "body" ).addClass( "show-mobile-menu" );    
+  } else {
+      Q.fcall( function(){
+        $( "#masthead" ).addClass( "hide-menu" );
+        $( "body" ).removeClass( "show-mobile-menu" );
+        return Q.delay( 300 );
+      }).then(function() {
+        $( "#masthead" ).removeClass( "hide-menu" ).removeClass( "show-menu" );
+      })
+      .catch(function(error){
+      
+      })
+      .done();
+  }
+});
+
+
+/*
+$( "#mobile-menu-button" ).click( function() {
+  
+  var isMobileMenu = $( "body" ).hasClass( "show-mobile-menu" );
+
+  if( !isMobileMenu ){
     $( "#masthead" ).addClass( "show-menu" ).removeClass("hide-menu");
     $( "body" ).addClass( "show-mobile-menu" );    
   } else {
@@ -12,7 +36,7 @@ $( "#mobile-menu-button" ).click( function() {
         $( "#masthead" ).removeClass("show-menu");
       });     
   }
-});
+});*/
 
 //   if ( !isMobileMenu ) {
     
