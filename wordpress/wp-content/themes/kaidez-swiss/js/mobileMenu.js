85f89a7e3874441944b6ca2cdc8c9e2197ee9baa
Kai Gittens <kai.gittens@gmail.com>
2015-07-08 22:59:55 -0400

    update mobile menu JS code

--- wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
+++ wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
@@ -1,7 +1,25 @@
+// Menu button
 $( "#mobile-menu-button" ).click( function() {
-  
-  var isMobileMenu = $( "body" ).hasClass( "show-mobile-menu" ),
-      isSearchMenu = $( "body" ).hasClass( "show-mobile-menu" );
+
+  if( $( "body" ).hasClass( "show-mobile-menu" ) ) {
+
+    Q.fcall( function(){
+      $( "#masthead" ).addClass( "hide-menu" );
+      $( "body" ).removeClass( "show-mobile-menu" );
+      return Q.delay( 300 );
+    }).then(function() {
+      $( "#masthead" ).removeClass( "hide-menu" ).removeClass( "show-menu" );
+    });
+
+  } else {
+
+    $( "#masthead" ).addClass( "show-menu" ).removeClass( "hide-menu" );
+    $( "body" ).addClass( "show-mobile-menu" );
+  }
+});
+
+// Search button
+$( "#mobile-search-button" ).click( function() {
 
   if( !isMobileMenu ){
     $( "#masthead" ).addClass( "show-menu" ).removeClass( "hide-menu" );
@@ -16,43 +34,3 @@ $( "#mobile-menu-button" ).click( function() {
       });
   }
 });
-
-
-/*
-$( "#mobile-menu-button" ).click( function() {
-  
-  var isMobileMenu = $( "body" ).hasClass( "show-mobile-menu" );
-
-  if( !isMobileMenu ){
-    $( "#masthead" ).addClass( "show-menu" ).removeClass("hide-menu");
-    $( "body" ).addClass( "show-mobile-menu" );    
-  } else {
-    $( "#masthead" ).addClass("hide-menu");
-    $( "body" ).removeClass( "show-mobile-menu" ).promise().done(
-      function(){
-        $( "#masthead" ).removeClass("show-menu");
-      });     
-  }
-});*/
-
-//   if ( !isMobileMenu ) {
-    
-//     $( "#masthead" ).addClass( "show-menu" );
-
-//     $( "body" ).addClass( "show-mobile-menu" );
-
-//   } else {
-
-//     $( "#masthead" ).addClass( "hide-menu" ).promise().done(
-//       function(){
-//         $( "#masthead" ).removeClass( "show-menu" );
-//       })
-//     .done(function(){
-//       $( "body" ).removeClass( "show-mobile-menu" );
-//     })
-//     .done(function(){
-      
-//     });
-//   }
-
-// });
