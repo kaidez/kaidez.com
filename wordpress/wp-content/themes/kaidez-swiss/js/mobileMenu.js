23bc841706398b0c6398dcc5540db3a312628bb0
Kai Gittens <kai.gittens@gmail.com>
2015-07-09 06:03:43 -0400

    update searchbox button fuctionality

--- wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
+++ wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
@@ -21,16 +21,19 @@ $( "#mobile-menu-button" ).click( function() {
 // Search button
 $( "#mobile-search-button" ).click( function() {
 
-  if( !isMobileMenu ){
-    $( "#masthead" ).addClass( "show-menu" ).removeClass( "hide-menu" );
-    $( "body" ).addClass( "show-mobile-menu" );    
+  if( $( "body" ).hasClass( "show-mobile-search" ) ) {
+
+    Q.fcall( function(){
+      $( "#searchform" ).addClass( "hide-searchbox" );
+      $( "body" ).removeClass( "show-mobile-search" );
+      return Q.delay( 300 );
+    }).then(function() {
+      $( "#searchform" ).removeClass( "hide-searchbox" ).removeClass( "show-searchbox" );
+    });
+
   } else {
-      Q.fcall( function(){
-        $( "#masthead" ).addClass( "hide-menu" );
-        $( "body" ).removeClass( "show-mobile-menu" );
-        return Q.delay( 300 );
-      }).then(function() {
-        $( "#masthead" ).removeClass( "hide-menu" ).removeClass( "show-menu" );
-      });
+
+    $( "#searchform" ).addClass( "show-searchbox" ).removeClass( "hide-searchbox" );
+    $( "body" ).addClass( "show-mobile-search" );
   }
 });
