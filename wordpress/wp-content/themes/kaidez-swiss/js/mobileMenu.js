18167326230a97ee3868e7d60b90f5f3492a81d2
Kai Gittens <kai.gittens@gmail.com>
2015-07-14 06:05:10 -0400

    commit working dynamic navigation code

--- wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
+++ wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
@@ -1,5 +1,46 @@
-var getHeader = document.getElementById( "masthead" );
+var nav = {
+  bodyClass: "show-mobile-menu",
+  targetEl: "#masthead",
+  hideClass: "hide-menu",
+  showClass: "show-menu"
+};
 
+function removeMobileClass() {
+  if ( $( "body" ).hasClass( "show-mobile-search" ) ) {
+    $( "body" ).removeClass( "show-mobile-search" );
+    $( "#searchform" ).addClass( "hide-searchbox" ).removeClass( "show-searchbox" );    
+  }
+}
+
+function animateNavElement( obj ) {
+  for ( var key in obj ) {
+    if(obj.hasOwnProperty(key)) {
+      if( $( "body" ).hasClass( obj.bodyClass ) ) {
+
+        Q.fcall( function(){
+          $( obj.targetEl ).addClass( obj.hideClass );
+          $( "body" ).removeClass( obj.bodyClass );
+          return Q.delay( 500 );
+        }).then( function(){
+          $( obj.targetEl ).removeClass( obj.hideClass ).removeClass( obj.showClass );
+        });
+      } else {
+        Q.fcall( function(){
+         removeMobileClass();
+      }).then(function(){
+          $( "#masthead" ).addClass( "show-menu" );
+          $( "body" ).addClass( "show-mobile-menu" );
+          $( "#searchform" ).removeClass( "hide-searchbox" )
+        });
+      }
+    }
+  }
+}
+
+$( "#mobile-menu-button" ).click(function(){
+  animateNavElement( nav );
+});
+/*
 // Menu button
 $( "#mobile-menu-button" ).click( function() {
 
@@ -29,7 +70,7 @@ $( "#mobile-menu-button" ).click( function() {
   }
 
 });
-
+*/
 // Search button
 $( "#mobile-search-button" ).click( function() {
 
