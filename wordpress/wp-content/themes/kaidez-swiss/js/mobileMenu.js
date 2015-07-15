052aa2d2145beae1395555a45fe34f835ed6a39e
Kai Gittens <kai.gittens@gmail.com>
2015-07-14 22:27:06 -0400

    add dynamic search box code

--- wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
+++ wordpress/wp-content/themes/kaidez-swiss/js/mobileMenu.js
@@ -4,7 +4,7 @@ var nav = {
   targetEl: "#masthead",
   hideClass: "hide-menu",
   showClass: "show-menu",
-  removeClassCheck: function(){
+  removeClassCheck: function() {
     if ( $( "body" ).hasClass( "show-mobile-search" ) ) {
       $( "body" ).removeClass( "show-mobile-search" );
       $( "#searchform" ).addClass( "hide-searchbox" ).removeClass( "show-searchbox" );    
@@ -12,6 +12,23 @@ var nav = {
   },
   singleRemoveClass: function() {
     $( "#searchform" ).removeClass( "hide-searchbox" );
+  }
+};
+
+// search code
+var search = {
+  bodyClass: "show-mobile-search",
+  targetEl: "#searchform",
+  hideClass: "hide-searchbox",
+  showClass: "show-searchbox",
+  removeClassCheck: function() {
+    if ( $( "body" ).hasClass( "show-mobile-menu" ) ) {
+      $( "body" ).removeClass( "show-mobile-menu" );
+      $( "#masthead" ).addClass( "hide-menu" ).removeClass( "show-menu" );    
+    }
+  },
+  singleRemoveClass: function() {
+    $( "#masthead" ).removeClass( "hide-menu" );
   }
 };
 
@@ -40,31 +57,7 @@ $( "#mobile-menu-button" ).click(function(){
   animateNavElement( nav );
 });
 
-
 // Search button
 $( "#mobile-search-button" ).click( function() {
-
-  if( $( "body" ).hasClass( "show-mobile-search" ) ) {
-
-    Q.fcall( function(){
-      $( "#searchform" ).addClass( "hide-searchbox" );
-      $( "body" ).removeClass( "show-mobile-search" );
-      return Q.delay( 300 );
-    }).then( function() {
-      $( "#searchform" ).removeClass( "hide-searchbox" ).removeClass( "show-searchbox" );
-    });
-
-  } else {
-
-    Q.fcall(function(){
-      if ( $( "body" ).hasClass( "show-mobile-menu" ) ) {
-        $( "#masthead" ).addClass( "hide-menu" ).removeClass( "show-menu" );
-        $( "body" ).removeClass( "show-mobile-menu" );
-      }
-    }).then( function(){
-      $( "#searchform" ).addClass( "show-searchbox" ).removeClass( "hide-searchbox" );
-      $( "body" ).addClass( "show-mobile-search" );
-      $( "#masthead" ).removeClass( "hide-menu" );
-    });
-  }
+  animateNavElement( search );
 });
