80207192034e2fd7f8123ab9c87b3db76dc9704d
Kai Gittens <kai.gittens@gmail.com>
2015-07-20 05:03:21 -0400

    hard-code the meta charset

--- wordpress/wp-content/themes/kaidez-swiss/header.php
+++ wordpress/wp-content/themes/kaidez-swiss/header.php
@@ -10,7 +10,7 @@
 ?><!DOCTYPE html>
 <html <?php language_attributes(); ?> class="no-js">
 <head>
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="UTF-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
   <link rel="profile" href="http://gmpg.org/xfn/11">
   <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
