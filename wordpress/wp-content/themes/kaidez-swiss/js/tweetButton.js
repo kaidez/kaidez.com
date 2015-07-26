82841d9339c20c55850efb6ebdb0551243dcd13e
Kai Gittens <kai.gittens@gmail.com>
2015-07-26 16:49:26 -0400

    update tweetButton code

--- wordpress/wp-content/themes/kaidez-swiss/js/tweetButton.js
+++ wordpress/wp-content/themes/kaidez-swiss/js/tweetButton.js
@@ -1,15 +1,24 @@
 // Based on cool-ass code at http://gpiot.com/blog/elegant-twitter-share-button-and-dialog-with-jquery/
 
-// We bind a new event to our link
-$("#tweet-this-post").click(function(e){
+(function(){
 
-  //We tell our browser not to follow that link
-  e.preventDefault();
+  var linkElement = document.getElementById( "tweet-this-post" ),
+      getPostTitle = document.getElementById( "blog-post-title" ).innerHTML,
+      getPostLink = window.location.href,
+      cleanLink = getPostLink.replace( /[^/]*$/g, "" );
 
-  var getPostTitle = document.getElementById('blog-post-title').innerHTML,
-    getPostLink = window.location.href;
+      linkElement.setAttribute( "href", cleanLink );
 
-  //Trigger a new window with the Twitter dialog
-  window.open('http://twitter.com/share?url=' + getPostLink + '&text=' + getPostTitle + '&', 'twitterwindow', 'height=450, width=550, top='+($(window).height()/2 - 225) +', left='+$(window).width()/2 +', toolbar=0, location=0, menubar=0, directories=0, scrollbars=0');
+  // Bind jQuery.click() to the Tweet link
+  $( linkElement ).on( "click", function( e ){
 
-});
+    //We tell our browser not to follow that link
+    e.preventDefault();
+
+    var tweetedLink = this.getAttribute( "href" );
+
+    window.open( "http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0" );
+
+  });
+
+})();
