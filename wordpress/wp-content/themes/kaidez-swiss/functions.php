7b107770aa90d1289287b7c71202e15d82ff121b
Kai Gittens <kai.gittens@gmail.com>
2015-08-06 20:52:50 -0400

    dequeue certain wordpress .js files

--- wordpress/wp-content/themes/kaidez-swiss/functions.php
+++ wordpress/wp-content/themes/kaidez-swiss/functions.php
@@ -114,9 +114,9 @@ add_action( 'widgets_init', 'kaidez_swiss_widgets_init' );
 function kaidez_swiss_scripts() {
 	wp_enqueue_style( 'kaidez-swiss-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'kaidez-swiss-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+	wp_dequeue_script( 'kaidez-swiss-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
 
-	wp_enqueue_script( 'kaidez-swiss-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+	wp_dequeue_script( 'kaidez-swiss-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
@@ -148,3 +148,6 @@ require get_template_directory() . '/inc/customizer.php';
  * Load Jetpack compatibility file.
  */
 require get_template_directory() . '/inc/jetpack.php';
+
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
