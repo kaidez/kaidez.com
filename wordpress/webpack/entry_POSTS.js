173a15cf2a18085b3652099dac480a195f8775fa
Kai Gittens <kai.gittens@gmail.com>
2015-08-09 16:16:59 -0400

    update entry_POSTS.js webpack build-out

--- wordpress/webpack/entry_POSTS.js
+++ wordpress/webpack/entry_POSTS.js
@@ -1,17 +1,23 @@
 var $ = require("jquery"),
     prism = require("./config/prism"),
+    sharin = require("./config/async-sharing"),
     comment = require("./config/comment-reply");
 
+// Require styles related to single posts ONLY!!!!
 require( "style!css!./posts.css" ); 
 
-var linkElement = document.getElementById( "tweet-this-post" );
+
+// START "CLICK-TO-TWEET" CODE
+
+var linkElement = document.getElementById( "tweet-this-post" ),
+    getPostLink = window.location.href;
 
 // Bind jQuery.click() to the Tweet link
 $( linkElement ).on( "click", function( event ){
   event.preventDefault();
   
   var getPostTitle = document.getElementById( "blog-post-title" ).innerHTML,
-      getPostLink = window.location.href,
+      // getPostLink = window.location.href,
       cleanLink = getPostLink.replace( /[^/]*$/g, "" ),
       tweetedLink;
 
@@ -22,3 +28,25 @@ $( linkElement ).on( "click", function( event ){
   window.open( "http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0" );
 
 });
+// END "CLICK-TO-TWEET" CODE
+
+
+
+// START FACEBOOK & GOOGLE+ SHARING CODE
+
+document.addEventListener("DOMContentLoaded", function(event) {
+
+  var socialSiteLinks = {
+    "facebook" : "facebook-share-link",
+    "googlePlus": "googleplus-share-link" 
+  },
+  linkId;
+
+  Object.getOwnPropertyNames( socialSiteLinks ).forEach(function( value ) {
+    linkId = socialSiteLinks[value];
+    console.log( getPostLink );
+    var pageElement = document.getElementById( linkId );
+  });
+
+});
+// END FACEBOOK & GOOGLE+ SHARING CODE
