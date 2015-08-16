5380cf9ac166fac44da5577ccf66bc733344a96e
Kai Gittens <kai.gittens@gmail.com>
2015-08-16 15:54:22 -0400

    get jQuery JSON call working

--- wordpress/webpack/entry_HOMEPAGE.js
+++ wordpress/webpack/entry_HOMEPAGE.js
@@ -7,3 +7,14 @@
 
 var $ = require( "jquery" ), // require jQuery
     q = require( "Q" ); // require the Q Promise library
+
+document.addEventListener( "DOMContentLoaded", function( event ) {
+
+  $.getJSON( "/wp-json", function( data ) {
+    console.log("getting!");
+  }).then(function(){
+    console.log("got it!");
+  });
+
+}); // end addEventListener call
+
