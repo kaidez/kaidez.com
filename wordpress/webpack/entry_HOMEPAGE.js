00d60ee8632e1a115c07e9f00bb06a7f6181fe74
Kai Gittens <kai.gittens@gmail.com>
2015-08-21 23:04:09 -0400

    start confirguring the posts JSON data on the home page

--- wordpress/webpack/entry_HOMEPAGE.js
+++ wordpress/webpack/entry_HOMEPAGE.js
@@ -8,7 +8,15 @@
 var $ = require( "jquery" ), // require jQuery
     q = require( "Q" ); // require the Q Promise library
 
+// Wait for the DOM to be ready before loading in JSON
 document.addEventListener( "DOMContentLoaded", function( event ) {
 
+  /*
+   * Load in post content with $.getJSON()
+   * Refer to the post content as "posts" inside the $.getJSON call
+   */
+  $.getJSON( "/wp-json/posts" ).done( function( posts ) {
+    console.log( posts[0].excerpt );
+  });
 
 }); // end addEventListener call
