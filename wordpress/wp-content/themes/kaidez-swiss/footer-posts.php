28e0af42af021bad1ec58440c19fec5d03144c0b
Kai Gittens <kai.gittens@gmail.com>
2015-08-21 21:45:23 -0400

    update script tags in WordPress footers

--- wordpress/wp-content/themes/kaidez-swiss/footer-posts.php
+++ wordpress/wp-content/themes/kaidez-swiss/footer-posts.php
@@ -38,7 +38,6 @@
 </div> <!-- #content -->
 
 <script src="/wp-content/themes/kaidez-swiss/js/common.js"></script>
-<script src="/wp-content/themes/kaidez-swiss/js/globals.js"></script>
 <script src="/wp-content/themes/kaidez-swiss/js/posts.js"></script>
 
 </body>
