f3fa9e85a0617476777da5deddddf673b4c70cf5
Kai Gittens <kai.gittens@gmail.com>
2015-08-23 08:37:28 -0400

    add comments to "webpack/entry_ASIDE.js"

--- wordpress/webpack/entry_ASIDE.js
+++ wordpress/webpack/entry_ASIDE.js
@@ -8,8 +8,8 @@
 
 var $ = require( "jquery" ), // require jQuery
     q = require( "Q" ), // require the Q Promise library
-    enquire = require("enquire.js"), // require.js media query library
-    getAside = "/wp-content/themes/kaidez-swiss/js/aside-code.html";
+    enquire = require("enquire.js"); // require.js media query library
+    
 
 // Load <aside>-specific CSS, which is preprocessed out with SASS.
 require("./config/aside.scss");
@@ -34,36 +34,47 @@ document.addEventListener( "DOMContentLoaded", function( event ) {
     // Run "getSidebar()", which loads the footer content
     getSidebar();
   }, function ( xhr ) {
-    
+
     // If the Promise fails, send a certain console message
    console.log( "The aside failed to load...you may needs refresh the page." );
   });
 
 });
 
+// getSidebar(): loads sidebar on all pages using enquire.js
 function getSidebar() {
 
+  // Variable reference to the footer content that gets AJAX'ed in
+  var getAside = "/wp-content/themes/kaidez-swiss/js/aside-code.html";
+
+  // Set a base media query value that enquire.js always checks
   enquire.register( "(min-width: 768px)", {
 
+    // On pageload, load in content via AJAX just once & hide it
     setup : function() {
 
-      // Load in content via AJAX (just the once)
       $( "#aside-id" ).load( getAside ).addClass( "aside-hide" );
       
     },
 
+    /*
+     * If the viewport matches the base media query value, toggle
+     * show/hide so the <aside> is SHOWN
+     */
     match : function() {
       
-      // Show sidebar
       $( "#aside-id" )
         .addClass( "aside-show" )
         .removeClass( "aside-hide" );
           
     },
 
+    /*
+     * If the viewport matches the base media query value, toggle
+     * show/hide so the <aside> is HIDDEN
+     */
     unmatch : function() {
-      
-      // Hide sidebar
+
       $( "#aside-id" )
         .addClass( "aside-hide" )
         .removeClass( "aside-show" );
