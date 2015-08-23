4cffc0f628493d72aa165964279c7715d7b55cd3
Kai Gittens <kai.gittens@gmail.com>
2015-08-23 07:50:56 -0400

    add comments to "webpack/entry_ASIDE.js"

--- wordpress/webpack/entry_ASIDE.js
+++ wordpress/webpack/entry_ASIDE.js
@@ -1,7 +1,8 @@
 /**
  * entry_SHARING.js
  *
- * Module for loading styles and scripts related to single posts only
+ * Module for loading styles/scripts loading related to the loading in
+ * the aside/footer content via AJAX
  *
  */
 
@@ -10,15 +11,30 @@ var $ = require( "jquery" ), // require jQuery
     enquire = require("enquire.js"), // require.js media query library
     getAside = "/wp-content/themes/kaidez-swiss/js/aside-code.html";
 
+// Load <aside>-specific CSS, which is preprocessed out with SASS.
 require("./config/aside.scss");
 
 // Wait for the DOM to be ready before loading content
 document.addEventListener( "DOMContentLoaded", function( event ) {
 
+  /*
+   * kaidez.com uses jQuery 2.1.4, which use Promises that don't
+   * conform to the Promises/A+ spec. wrap it in a Q return makes
+   * jQuery spec-complaint. Note that Promises in jQuery 3.x are
+   * Promises/A+ compliant.
+   */
+
+  /*
+   * Remove the AJAX-y animated .gif that's already on the page, then
+   * return a Promise
+   */
   return q( $( ".loading" ).remove() )
   .then(function(){
+    
+    // Run "getSidebar()", which loads the footer content
     getSidebar();
   }, function ( xhr ) {
+    
     // If the Promise fails, send a certain console message
    console.log( "The aside failed to load...you may needs refresh the page." );
   });
@@ -32,7 +48,7 @@ function getSidebar() {
     setup : function() {
 
       // Load in content via AJAX (just the once)
-      $( "#aside-id" ).load( getAside ).addClass( "aside-hide" )
+      $( "#aside-id" ).load( getAside ).addClass( "aside-hide" );
       
     },
 
@@ -57,5 +73,3 @@ function getSidebar() {
   });
 
 }
-
-
