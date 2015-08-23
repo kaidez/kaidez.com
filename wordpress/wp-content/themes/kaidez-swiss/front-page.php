4ef22741b491da8ebf7842b9f57c65ad3c9e8fbf
Kai Gittens <kai.gittens@gmail.com>
2015-08-23 07:21:05 -0400

    clean up uneeded php, add <section> to the front page

--- wordpress/wp-content/themes/kaidez-swiss/front-page.php
+++ wordpress/wp-content/themes/kaidez-swiss/front-page.php
@@ -15,33 +15,10 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
   <main id="main" class="site-main" role="main" itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+    <section id="all-articles"></section>
 
-    <?php if ( have_posts() ) : ?>
 
-      <?php /* Start the Loop */ ?>
-      <?php while ( have_posts() ) : the_post(); ?>
-
-        <?php
-
-          /*
-           * Include the Post-Format-specific template for the content.
-           * If you want to override this in a child theme, then include a file
-           * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-           */
-          get_template_part( 'template-parts/content', get_post_format() );
-          ?>
-
-        <?php endwhile; ?>
-
-        <?php the_posts_navigation(); ?>
-
-      <?php else : ?>
-
-        <?php get_template_part( 'template-parts/content', 'none' ); ?>
-
-      <?php endif; ?>
-
-    </main><!-- #main -->
+  </main><!-- #main -->
   </div><!-- #primary -->
 
   <?php get_sidebar(); ?>
