2acfc5327abd0db51873ba0a225339a2562d2595
Kai Gittens <kai.gittens@gmail.com>
2015-08-25 13:25:09 -0400

    remove globals.js, add regular.js to regular footer.php

--- wordpress/wp-content/themes/kaidez-swiss/footer.php
+++ wordpress/wp-content/themes/kaidez-swiss/footer.php
@@ -20,7 +20,7 @@
 </div> <!-- #content -->
 
 <script src="/wp-content/themes/kaidez-swiss/js/common.js"></script>
-<script src="/wp-content/themes/kaidez-swiss/js/globals.js"></script>
+<script src="/wp-content/themes/kaidez-swiss/js/regular.js"></script>
 
 </body>
 </html>
