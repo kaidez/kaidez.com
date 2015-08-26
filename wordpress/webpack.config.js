7f5e129f3411bca49a9ca237beb97b97b31f06b3
Kai Gittens <kai.gittens@gmail.com>
2015-08-26 10:23:29 -0400

    add comments to "webpack.config.js"

--- wordpress/webpack.config.js
+++ wordpress/webpack.config.js
@@ -1,23 +1,35 @@
+// Set up some variables to get webpack working...
+
+// require webpack so plugins can be used
 var webpack = require( "webpack" ),
+    
+    /*
+     * Bring in the common chunks plugin so shared JS code is broken
+     * out into a file called "common.js"
+     */
     commonsChunkPlugin = new webpack.optimize.CommonsChunkPlugin( "common.js" ),
+    
+    // Bring in the uglify plugin to minify all .js files
     UglifyJsPlugin = new webpack.optimize.UglifyJsPlugin({
       compress: {
         warnings: false
       }
     });
 
+// Configure webpack
 module.exports = {
 
+  // Define multiple entry points to build out multiple files
   entry: {
     
-    // Create a file called "home.js" with specific modules
+    // Compile out a "home.js" file with specific modules
     home: [
       "./webpack/entry_GLOBALS",
       "./webpack/entry_ASIDE",
       "./webpack/entry_HOMEPAGE",
       ],
     
-    // Create a file called "posts.js" with specific modules
+    // Compile out a "posts.js" file with specific modules
     posts: [
       "./webpack/entry_GLOBALS",
       "./webpack/entry_ASIDE",
@@ -27,7 +39,7 @@ module.exports = {
       "./webpack/config/comment-reply"
     ],
 
-    // Create a file called "regular.js" with specific modules
+    // Compile out a "regular.js" file with specific modules
     regular: [
       "./webpack/entry_GLOBALS",
       "./webpack/entry_ASIDE"
@@ -39,15 +51,21 @@ module.exports = {
     filename: "[name].js"
   },
 
+  // Setup modules, which are basically plugins for webpack
   module: {
     loaders: [
+      
+      // Load style modules into a <style> tag
       {test: /\.css$/, loader: "style-loader!css-loader"},
+      
+      // Preprocess SASS files
       {test: /\.scss$/,loader: 'style!css!sass'}
     ]
   },
 
+  // Configure some internal webpack modules as plugins
   plugins: [
-    commonsChunkPlugin,
-    UglifyJsPlugin
+    commonsChunkPlugin, // Split out shared JS code into common chunks
+    UglifyJsPlugin // Minify the .js files that webpack builds out
   ]
 };
