ea3ee23c0b0fd3ef0223f358d8b7b4ecfc39f889
Kai Gittens <kai.gittens@gmail.com>
2015-08-27 17:06:36 -0400

    style "category" links on the home page

--- wordpress/webpack/entry_HOMEPAGE.js
+++ wordpress/webpack/entry_HOMEPAGE.js
@@ -5,6 +5,8 @@
  *
  */
 
+ "use strict"; 
+
 var $ = require( "jquery" ); // require jQuery
 
 require( "./homepage.scss" ); // styles for the homepage only
@@ -22,113 +24,120 @@ document.addEventListener( "DOMContentLoaded", function( event ) {
    * Grab the first 9 single posts from the WordPress API & sort them 
    * by date.
    */
-  var getPostsAPI = "/wp-json/posts?filter[orderby]=date&filter[posts_per_page]=9";
+    var getPostsAPI = "/wp-json/posts?filter[orderby]=date&filter[posts_per_page]=9";
 
   /*
    * Load in post content data with $.getJSON()
    * Refer to this data as "posts" inside the $.getJSON call
    */
-  $.getJSON( getPostsAPI ).done( function( posts ) {
-    
-    // var reference to <section id="all-articles" /> on the home page
-    var articleSection = document.getElementById( "all-articles" ),
-        
+    $.getJSON( getPostsAPI ).done( function( posts ) {
+
+      // var reference to <section id="all-articles"> on the home page
+      var articleSection = document.getElementById( "all-articles" ),
+
       /*
        * Create document fragment to batch-load a single snippet onto
        * the page
        */
       sectionDocFrag = document.createDocumentFragment();
 
-    for( var key in posts ) {
+      for( var outerKey in posts ) {
 
-      // jQuery-styled single var pattern
-      var postImage, postLink, postTitle, postExcerpt, postCategory,
-          snippetContainer = document.createElement( "article" ),
-          snippetHeader = document.createElement( "h2" ),
-          singleExcerpt = document.createElement( "p" ),
-          snippetLink = document.createElement( "a" ),
-          snippetImage = document.createElement( "img" ),
-          imageDiv = document.createElement( "div" ),
-          categoryDiv = document.createElement( "div" );
+        // Run hasOwnPropertyCheck for the outer for..in loop
+        if( posts.hasOwnProperty(outerKey) ) {
 
-      // SET UP THE <article> TAG!!!!
+          // jQuery-styled single var pattern
+          var postImage,
+              postLink,
+              postTitle,
+              postExcerpt,
+              postCategory,
+              categoryHref,
+              snippetContainer = document.createElement( "article" ),
+              snippetHeader = document.createElement( "h2" ),
+              singleExcerpt = document.createElement( "p" ),
+              snippetLink = document.createElement( "a" ),
+              snippetImage = document.createElement( "img" ),
+              imageDiv = document.createElement( "div" ),
+              categoryLink = document.createElement( "a" );
 
-      // Check if we're working with the first post
-      if( posts[key] == posts[0] ) {
-        /* 
-         * If it's the first post, apply the
-         * "first-homepage-post-snippet" class to the <article> tag
-         */
-        snippetContainer.setAttribute( "class", "first-homepage-post-snippet" );
-      } else {
-        /* 
-         * If it's NOT the first post, apply the
-         * "homepage-post-snippet" class to the <article> tag
-         */
-        snippetContainer.setAttribute( "class", "homepage-post-snippet" );        
-      }
-      // STOP <article> TAG SETUP!!!!
+          // SET UP THE <article> TAG!!!!
 
-
-
-      // SET UP SINGLE BLOG POST IMAGES!!!!
-
-      /*
-       * Featured images are defined by "Featured Image" section of a
-       * single WordPress post's page. If it's not defined, a
-       * default image is loaded via the "Default featured image"
-       * WordPress plugin.
-       */
-      postImage = posts[key].featured_image["source"];
-
-      // Add styles and a "src" attribute to the image 
-      $( snippetImage ).attr({
-        "src": postImage,
-        "class": "post-pic post-pic-border"
-      });
-
-      // Load post image in a <div>
-      imageDiv.appendChild( snippetImage ); 
+          // Check if we're working with the first post
+          if( posts[outerKey] == posts[0] ) {
+            /* 
+             * If it's the first post, apply the
+             * "first-homepage-post-snippet" class to the <article> tag
+             */
+            snippetContainer.setAttribute( "class", "first-homepage-post-snippet" );
+          } else {
+            /* 
+             * If it's NOT the first post, apply the
+             * "homepage-post-snippet" class to the <article> tag
+             */
+            snippetContainer.setAttribute( "class", "homepage-post-snippet" );        
+          }
+          // STOP <article> TAG SETUP!!!!
 
 
 
-      // STOP SINGLE BLOG POST IMAGES SETUP!!!!
+          // SET UP SINGLE BLOG POST IMAGES!!!!
+
+          /*
+           * Featured images are defined by "Featured Image" section of a
+           * single WordPress post's page. If it's not defined, a
+           * default image is loaded via the "Default featured image"
+           * WordPress plugin.
+           */
+          postImage = posts[outerKey].featured_image.source;
+
+          // Add styles and a "src" attribute to the image 
+          $( snippetImage ).attr({
+            "src": postImage,
+            "class": "post-pic post-pic-border"
+          });
+
+          // Load post image in a <div>
+          imageDiv.appendChild( snippetImage ); 
+
+          // STOP SINGLE BLOG POST IMAGES SETUP!!!!
 
 
 
-      // SET UP SINGLE BLOG LINK & HEADER!!!!
+          // SET UP SINGLE BLOG LINK & HEADER!!!!
 
-      // Get post title link
-      postLink = posts[key].link;
+          // Get post title link
+          postLink = posts[outerKey].link;
 
-      // Get post title copy
-      postTitle = posts[key].title;
+          // Get post title copy
+          postTitle = posts[outerKey].title;
 
-      // Set the article link's "href" to be the post link
-      $(snippetLink).attr({
-        "href": postLink,
-        class: "post-link"
-      }); 
+          // Set the article link's "href" to be the post link
+          $(snippetLink).attr({
+            "href": postLink,
+            class: "post-link"
+          }); 
 
-      // Load post title copy in the <a> tag
-      snippetLink.innerHTML = postTitle;
+          // Load post title copy in the <a> tag
+          snippetLink.innerHTML = postTitle;
 
-      // Load link into the post header
-      snippetHeader.appendChild( snippetLink ); 
+          // Load link into the post header
+          snippetHeader.appendChild( snippetLink ); 
 
-      // STOP SINGLE BLOG LINK & HEADER SETUP!!!!
+          // STOP SINGLE BLOG LINK & HEADER SETUP!!!!
 
 
 
-      // SET UP SINGLE BLOG POST EXCERPT!!!!
+          // SET UP SINGLE BLOG POST EXCERPT!!!!
       
       // Get post excerpt copy
-      postExcerpt = posts[key].excerpt;
+      postExcerpt = posts[outerKey].excerpt;
 
       // Load post excerpt copy in the <p> tag
       singleExcerpt.innerHTML = postExcerpt;
 
       // STOP SINGLE BLOG POST EXCERPT SETUP!!!!
+
 
 
       /*
@@ -152,39 +161,47 @@ document.addEventListener( "DOMContentLoaded", function( event ) {
       /*
        * SET UP THE CATEGORY !!!!
        *
-       * The category name object is nested deeper in the WP-API than
-       * the other info, so we have to do a an inner for...in loop to
-       * find it. This code needs to run last; otherwise, it breaks
-       * the process of grabbing certain data from the WP-API for all
-       * the posts.
+       * The category name object is nested deeper in the WP-API
+       * than the other info, so we have to do a an inner for...in
+       * loop to find it. This code needs to run last; otherwise,
+       * it breaks the process of grabbing post data.
        */
-      
+
       // Grab the "category" value in the WP-API
-      postCategory = posts[key].terms["category"];
+      postCategory = posts[outerKey].terms.category;
 
       // Loop through the category object
-      for ( var key in postCategory ) {
+      for ( var innerKey in postCategory ) {
 
-        // Find category name and put it in the "categoryDiv" element
-        categoryDiv.innerHTML = postCategory[key].name;
+        if( postCategory.hasOwnProperty( innerKey ) ){
 
-        // Give "categoryDiv" element a class name of "category-name"
-        categoryDiv.setAttribute( "class", "category-name" );
+            // Find category name & put it in "categoryLink" element
+            categoryLink.innerHTML = postCategory[innerKey].name;
+            categoryHref = "/" + postCategory[innerKey].slug + "/";
+
+            // Give "categoryLink" class name & href attributes
+            $(categoryLink).attr({
+              "class": "category-name",
+              "href": categoryHref
+            });
         
-        // Use $.prepend() to put "categoryDiv" at the TOP of <article>
-        $( snippetContainer ).prepend( categoryDiv ); 
+            // Use $.prepend() to put categoryLink at TOP of <article>
+            $( snippetContainer ).prepend( categoryLink ); 
+
+          }  // END INNER "for...in" LOOP's hasOwnProperty() check
+
+        }  // END INNER "for...in" LOOP
+
+        // STOP CATEGORY SETUP !!!!
+
+
+
+        /*
+         * Load article with title, image, excerpt and category into
+         * the document fragment
+         */
+        sectionDocFrag.appendChild( snippetContainer );
       }
-
-      // STOP CATEGORY SETUP !!!!
-
-
-
-      /*
-       * Load article with title, image, excerpt and category into the
-       * document fragment
-       */
-      sectionDocFrag.appendChild( snippetContainer );
-
     } // end for...in loop
 
     /* 
