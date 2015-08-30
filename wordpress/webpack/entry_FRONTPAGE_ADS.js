d5d9aa2013e32a43e095c549050a9c45cd89594c
Kai Gittens <kai.gittens@gmail.com>
2015-08-30 08:26:52 -0400

    continue building/testing dynamic add building for the home page

--- wordpress/webpack/entry_FRONTPAGE_ADS.js
+++ wordpress/webpack/entry_FRONTPAGE_ADS.js
@@ -18,9 +18,48 @@ define( ["jquery"], function( $ ) {
    * files via a forEach() loop
    */
   if ( !window.matchMedia ) {
-    matchMediaArray.forEach( function( index ){
+    return matchMediaArray.forEach( function( index ){
       $.getScript( index );
     });
   }
+
+  // TODO: all lynda ads need "border='0'" applied to the <img> tag
+  var ads = {
+      "lynda_gift" : {
+        "pageEl" : "ad-spot-one",
+        "link"   : "https://www.lynda.com/giftsubscription/index.aspx?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11900&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=11900&aid=CD2146",
+        "src" : "http://lynda.directtrack.com/42/2146/11900/",
+        "alt"    : "Give lynda.com"
+      },
+
+      "lynda_promo" : {
+        "pageEl" : "ad-spot-two",
+        "link"   : "http://www.lynda.com/promo/trial/Default.aspx?lpk35=7840&utm_medium=ldc-partner&utm_source=SSPRC&utm_content=753&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=753&aid=CD2146",
+        "src" : "http://lynda.directtrack.com/42/2146/753/",
+        "alt"    : "10-day free trial"
+      }
+    };
+
+  var buildAd = function(){
+    for (var keys in ads) {
+      return console.log(ads);
+    }
+
+  };
+
+  buildAd();
+  
+  // Set a base media query value that enquire.js always checks
+  enquire.register( "( min-width: 768px )", {
+
+    match : function() {},
+
+    unmatch : function() {},
+
+    setup : function() {},
+
+    deferSetup : true
+
+  });
 
 });
