68f77bbd22d24c2f221578b044643f6a0aca7f34
Kai Gittens <kai.gittens@gmail.com>
2015-08-29 21:11:15 -0400

    add comments

--- wordpress/webpack/entry_FRONTPAGE_ADS.js
+++ wordpress/webpack/entry_FRONTPAGE_ADS.js
@@ -7,11 +7,16 @@
 
 define(["jquery"], function( $ ) {
 
+  // matchMedia() polyfill files
   var matchMediaArray = [
         "/wp-content/themes/kaidez-swiss/js/libs/matchMedia.addListener.js",
         "/wp-content/themes/kaidez-swiss/js/libs/matchMedia.js"
       ];
 
+  /*
+   * If the browser doesn't support matchMedia(), load in polyfill
+   * files via a forEach() loop
+   */
   if ( !window.matchMedia ) {
     matchMediaArray.forEach( function( index ){
       $.getScript( index );
