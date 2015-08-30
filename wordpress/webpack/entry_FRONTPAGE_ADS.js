3434c54c83d55432e30dfae671a1c0a595399b5d
Kai Gittens <kai.gittens@gmail.com>
2015-08-30 15:20:38 -0400

    fix whitespace

--- wordpress/webpack/entry_FRONTPAGE_ADS.js
+++ wordpress/webpack/entry_FRONTPAGE_ADS.js
@@ -25,6 +25,7 @@ define( ["jquery"], function( $ ) {
 
   // TODO: all lynda ads need "border='0'" applied to the <img> tag
   var ads = {
+
       "lynda_gift" : {
         "pageEl" : "ad-spot-one",
         "link"   : "https://www.lynda.com/giftsubscription/index.aspx?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11900&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=11900&aid=CD2146",
@@ -38,6 +39,7 @@ define( ["jquery"], function( $ ) {
         "src" : "http://lynda.directtrack.com/42/2146/753/",
         "alt"    : "10-day free trial"
       }
+      
     };
 
   var buildAd = function() {
