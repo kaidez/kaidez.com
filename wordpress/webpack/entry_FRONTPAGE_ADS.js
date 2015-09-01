472a89c7bd96f74981e9fa5fa68e6811b2e2cdd3
Kai Gittens <kai.gittens@gmail.com>
2015-08-31 20:55:54 -0400

    update front-page add build-out code

--- wordpress/webpack/entry_FRONTPAGE_ADS.js
+++ wordpress/webpack/entry_FRONTPAGE_ADS.js
@@ -1,4 +1,4 @@
-/**
+/*
  * entry_FRONTPAGE_ADS.js
  *
  * Module for loading styles and scripts related to front page ads only
@@ -24,34 +24,32 @@ define( ["jquery"], function( $ ) {
   }
 
   // TODO: all lynda ads need "border='0'" applied to the <img> tag
-  var ads = {
-
-      "lynda_gift" : {
-        "pageEl" : "ad-spot-one",
-        "link"   : "https://www.lynda.com/giftsubscription/index.aspx?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11900&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=11900&aid=CD2146",
-        "src" : "http://lynda.directtrack.com/42/2146/11900/",
-        "alt"    : "Give lynda.com"
-      },
-
-      "lynda_promo" : {
-        "pageEl" : "ad-spot-two",
-        "link"   : "http://www.lynda.com/promo/trial/Default.aspx?lpk35=7840&utm_medium=ldc-partner&utm_source=SSPRC&utm_content=753&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=753&aid=CD2146",
-        "src" : "http://lynda.directtrack.com/42/2146/753/",
-        "alt"    : "10-day free trial"
-      }
-      
-    };
-
-  var buildAd = function() {
-
-    for (var keys in ads) {
-      return console.log(ads);
-    }
-
+  
+  // Lynda promo ad
+  var lyndaPromo = {
+        pageElement: "ad-spot-one",
+        link: "http://www.lynda.com/promo/trial/Default.aspx?lpk35=7840&utm_medium=ldc-partner&utm_source=SSPRC&utm_content=753&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=753&aid=CD2146",
+        imageSource: "http://lynda.directtrack.com/42/2146/753/",
+        alt: "10-day free trial"
   };
 
-  buildAd();
-  
+  // Lynda gift ad 
+  var lyndaGift = {
+        pageElement: "ad-spot-two",
+        link: "https://www.lynda.com/giftsubscription/index.aspx?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11900&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=11900&aid=CD2146",
+        imageSource : "http://lynda.directtrack.com/42/2146/11900/",
+        alt: "Give lynda.com"
+  };
+
+  function buildAd( obj ) {
+
+    var adPageTarget = obj.pageElement,
+        adLink = obj.link,
+        adImageSource = obj.imageSource,
+        adAltTag = obj.alt;
+  };
+
+
   // Set a base media query value that enquire.js always checks
   enquire.register( "( min-width: 768px )", {
 
@@ -65,4 +63,4 @@ define( ["jquery"], function( $ ) {
 
   });
 
-});
+}); // end "define()"
