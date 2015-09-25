bed768f375aff71d30bcb2d40aa0c0c5e5ab47f6
Kai Gittens <kai.gittens@gmail.com>
2015-09-25 12:33:22 -0400

    update HTML sitmap code

--- wordpress/webpack/entry_SITEMAP.js
+++ wordpress/webpack/entry_SITEMAP.js
@@ -13,35 +13,61 @@ document.addEventListener( "DOMContentLoaded", function( event ) {
   var getPosts = "/wp-json/posts", // post data
       getPages = "/wp-json/pages"; // page data
 
-  getSitemapData( getPosts );
-  getSitemapData( getPages );
+  getSitemapData( getPosts, "posts" );
+  getSitemapData( getPages, "pages" );
 
 }); // end addEventListener
 
 
-function getSitemapData( getdata ) {
+function getSitemapData( getdata, linkElement ) {
   /*
    * Load in post content data with $.getJSON()
    * Refer to this data as "data" inside the $.getJSON call
    */
   $.getJSON( getdata ).done( function( data ) {
-    
-    var getLink,
-        getTitle;
 
     for( var dataKey in data ) {
 
-      
       // Run hasOwnPropertyCheck for the for..in loop
       if( data.hasOwnProperty( dataKey ) ) {
 
-        getTitle = data[dataKey].title; // Get post/page title
-        getLink = data[dataKey].link; // Get post/page link
-        console.log(getTitle);
-        console.log(getLink);
-      } //End "for...in" loop's hasOwnProperty() check
+        var
+            // Get post/page link
+            getLink = data[dataKey].link;
 
-    } // End "for...in" LOOP 
+            // Get post/page title
+            getTitle = data[dataKey].title;
+
+            // create an <a> tag for the sitemap link
+            sitemapLink = document.createElement( "a" );
+
+            // create a <p> tag for the sitemap link
+            sitemapLinkParagragh = document.createElement( "p" );
+
+            // create a <p> tag for the sitemap link 
+            targetElement = document.getElementById( linkElement );
+
+            // create a document fragment tag for the sitemap link
+            docFrag = document.createDocumentFragment();
+
+            if( getTitle !== "Front Page" && getTitle !== "Site Map" )   {
+
+              sitemapLink.setAttribute( "href", getLink );
+              sitemapLink.innerHTML = getTitle;
+
+              sitemapLinkParagragh.appendChild( sitemapLink );
+              docFrag.appendChild( sitemapLinkParagragh );
+            }
+
+      } // end "for...in" loop's hasOwnProperty() check
+
+      /*
+       * Load the full document fragment in to the element defined by
+       * the targetElement
+       */
+      targetElement.appendChild( docFrag );
+
+    } // end "for...in" LOOP 
       
   }); // end $.getJSON()
 }
