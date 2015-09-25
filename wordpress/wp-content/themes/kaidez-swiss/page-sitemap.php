b89b2e19ebd2bb7172715724cae48b21e211d522
Kai Gittens <kai.gittens@gmail.com>
2015-09-25 11:56:12 -0400

    remove uneeded PHP code, add <h2> tags

--- wordpress/wp-content/themes/kaidez-swiss/page-sitemap.php
+++ wordpress/wp-content/themes/kaidez-swiss/page-sitemap.php
@@ -13,18 +13,13 @@ get_header(); ?>
   <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
 
-      <?php while ( have_posts() ) : the_post(); ?>
+    <div id="posts">
+      <h2>Posts</h2>
+    </div><!-- #posts -->
 
-        <?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-        <?php
-          // If comments are open or we have at least one comment, load up the comment template.
-          if ( comments_open() || get_comments_number() ) :
-            comments_template();
-          endif;
-        ?>
-
-      <?php endwhile; // End of the loop. ?>
+    <div id="pages">
+      <h2>Pages</h2>
+    </div><!-- #pages -->
 
     </main><!-- #main -->
   </div><!-- #primary -->
