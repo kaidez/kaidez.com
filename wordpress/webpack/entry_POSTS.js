2b1b10ad246c279623a9e28fbc9cd3d79356d968
Kai Gittens <kai.gittens@gmail.com>
2015-10-11 12:38:12 -0400

    create "webpack/entry_POSTS.js" for post page scripts only

---
+++ wordpress/webpack/entry_POSTS.js
@@ -0,0 +1,11 @@
+/**
+ * entry_POSTS.js
+ *
+ * Module for loading styles and scripts related to single posts only
+ *
+ */
+
+var $ = require( "jquery" ); // require jQuery
+
+$( ".entry-content p" ).first().css("display", "inline-table");
+    
