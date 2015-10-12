6a01c9bad18396d16d576452d91251d4dad17d02
Kai Gittens <kai.gittens@gmail.com>
2015-10-12 10:30:37 -0400

    add/update single post ad code

--- wordpress/webpack/config/postAds.js
+++ wordpress/webpack/config/postAds.js
@@ -8,8 +8,16 @@ module.exports = {
 
   "lyndaWebDev": "<a href=\"http://www.lynda.com/Web-Development-training-tutorials/1471-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11631&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:1471:ind:ssprc:CD2146&bid=11631&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/11631/\" alt=\"Web developer tutorials\" border=\"0\"></a>",
 
-  "lyndaTrial" : "<a href=\"http://www.lynda.com/promo/trial/Default.aspx?lpk35=7840&utm_medium=ldc-partner&utm_source=SSPRC&utm_content=755&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=755&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/755/\" alt=\"10-day free trial\" border=\"0\"></a>",
+  "lyndaTrial" : "<a href='http://www.lynda.com/promo/trial/Default.aspx?lpk35=7840&utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11949&utm_campaign=CD2146&cid=l0:en:af:le:prosb:s0:0:ind:ssprc:CD2146&bid=11949&aid=CD2146'><img src='http://lynda.directtrack.com/42/2146/11949/' alt=' border='0'></a>",
 
-  "lyndaWordpress" : "<a href=\"http://www.lynda.com/WordPress-training-tutorials/330-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11878&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:330:ind:ssprc:CD2146&bid=11878&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/11878/\" alt=\"Learn WordPress\" border=\"0\"></a>"
+  "lyndaWordpress" : "<a href=\"http://www.lynda.com/WordPress-training-tutorials/330-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11878&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:330:ind:ssprc:CD2146&bid=11878&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/11878/\" alt=\"Learn WordPress\" border=\"0\"></a>",
+
+    "mediaTempleDVManaged" : "<a href=\"http://mediatemple.net/webhosting/vps/hosting/#a_aid=5068b81963acf&amp;a_bid=21354d24\" target=\"_top\" rel=\"nofollow\"><img src=\"https://affiliate.mediatemple.net/accounts/default1/banners/300x250-1.jpg\" alt=\"\" width=\"300\" height=\"250\" /></a><img style=\"border:0\" src=\"https://affiliate.mediatemple.net/scripts/imp.php?a_aid=5068b81963acf&amp;a_bid=21354d24\" width=\"1\" height=\"1\" alt=\"\" />",
+
+    "mediaTempleDVDeveloper" : "<a href='http://mediatemple.net/webhosting/vps/developer/#a_aid=5068b81963acf&amp;a_bid=07810a77' target='_top' rel='nofollow'><img src='https://affiliate.mediatemple.net/accounts/default1/banners/300x250-2.jpg' alt=' width='300' height='250' /></a><img style='border:0' src='https://affiliate.mediatemple.net/scripts/imp.php?a_aid=5068b81963acf&amp;a_bid=07810a77' width='1' height='1' alt=' />",
+
+    "maxcdnBlack":"<a href='http://tracking.maxcdn.com/c/72639/133482/378'><img src='http://adn.impactradius.com/display-ad/378-133482' border='0' alt=' width='300' height='250'/></a><img height='0' width='0' src='http://tracking.maxcdn.com/i/72639/133482/378' style='position:absolute;visibility:hidden;' border='0' />",
+
+    "maxcdnOrange":"<a href='http://tracking.maxcdn.com/c/72639/133486/378'><img src='http://adn.impactradius.com/display-ad/378-133486' border='0' alt=' width='300' height='250'/></a><img height='0' width='0' src='http://tracking.maxcdn.com/i/72639/133486/378' style='position:absolute;visibility:hidden;' border='0' />"
 
 }
