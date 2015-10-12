796c61caea4d8cf5fe78bfaa2035619659f9c6f1
Kai Gittens <kai.gittens@gmail.com>
2015-10-12 06:46:34 -0400

    update "webpack/entry_POSTS.js" (see message) -require single post ads via "postAds.js" -create code that dynamically styles the view demo btn on single posts

--- wordpress/webpack/entry_POSTS.js
+++ wordpress/webpack/entry_POSTS.js
@@ -5,7 +5,24 @@
  *
  */
 
-var $ = require( "jquery" ); // require jQuery
+var $ = require( "jquery" ), // require jQuery
+    PostAds = require( "./config/postAds" ); // require ads
 
-$( ".entry-content p" ).first().css("display", "inline-table");
-    
+
+/*
+ * Dynamically position the first paragraph in a single post
+ * ====================================================================
+ */
+
+// Store a reference to the "view the demo" btn that may be on the page
+ var getDemoButton = document.querySelector( ".demo-link" );
+
+/*
+ * Pages with demo buttons are in the first <p> of a post page content
+ * element, which is ".entry-content". They throw off ad alignment so
+ * check to see if a post as this button and if it does, use jQuery to
+ * add styling that prevents the "throwing off"
+ */
+if ( getDemoButton ) {
+  $( ".entry-content p" ).first().css( "display", "inline-table" );
+}
