19fefe76989691eb856b1065311f5e442803fbd3
Kai Gittens <kai.gittens@gmail.com>
2015-10-12 07:53:56 -0400

    create code for loading ads onto posts, add comments

--- wordpress/webpack/entry_POSTS.js
+++ wordpress/webpack/entry_POSTS.js
@@ -26,3 +26,36 @@ var $ = require( "jquery" ), // require jQuery
 if ( getDemoButton ) {
   $( ".entry-content p" ).first().css( "display", "inline-table" );
 }
+
+
+
+
+/*
+ * LOAD ADS
+ * ====================================================================
+ * This code loops through an object the "PostAds" module that
+ * contains a listing of all the available ads. These ads appear at the
+ * top of the posts, where text wraps around them. The key in each
+ * post needs to match the ID of an element already on the page and if
+ * it does, the key's value is what gets load into this element
+ */
+
+/*
+ * Use a for/in loop against the object in "PostAds"
+ */
+for ( key in PostAds ) {
+
+  // Perform a standard hasOwnProperty check against the object
+  if( PostAds.hasOwnProperty( key ) ) {
+
+    var targetElement = document.getElementById( key );
+
+    if( !targetElement ) {
+      null;
+    } else {
+      targetElement.innerHTML = PostAds[key];
+    }
+
+  } // end the hasOwnProperty check
+
+} // end the for/in loop
