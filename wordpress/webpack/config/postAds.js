84ed90dc8f5d38b54224f10542ab10dadf561452
Kai Gittens <kai.gittens@gmail.com>
2015-10-12 06:49:45 -0400

    create "webpack/config/postAds.js" which contains ad data

---
+++ wordpress/webpack/config/postAds.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "lynda" : {
+    "lynda-javascript": "<a href=\"http://www.lynda.com/JavaScript-training-tutorials/244-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=598&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:244:ind:ssprc:CD2146&bid=598&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/598/\" alt=\"Learn Javascript\" border=\"0\"></a>",
+
+      "lynda-html5": "<a href=\"http://www.lynda.com/HTML-training-tutorials/224-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11866&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:224:ind:ssprc:CD2146&bid=11866&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/11866/\" alt=\"Learn HTML5\" border= \"0\"></a>",
+
+      "lynda-css": "<a href=\"http://www.lynda.com/CSS-training-tutorials/447-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=628&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:447:ind:ssprc:CD2146&bid=628&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/628/\" alt=\"Learn CSS\" border=\"0\"></a>",
+
+      "lynda-web-dev": "<a href=\"http://www.lynda.com/Web-Development-training-tutorials/1471-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11631&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:1471:ind:ssprc:CD2146&bid=11631&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/11631/\" alt=\"Web developer tutorials\" border=\"0\"></a>",
+
+      "lynda-trial" : "<a href=\"http://www.lynda.com/promo/trial/Default.aspx?lpk35=7840&utm_medium=ldc-partner&utm_source=SSPRC&utm_content=755&utm_campaign=CD2146&cid=l0:en:pt:le:prosb:s0:0:ind:ssprc:CD2146&bid=755&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/755/\" alt=\"10-day free trial\" border=\"0\"></a>",
+
+      "lynda-wordpress" : "<a href=\"http://www.lynda.com/WordPress-training-tutorials/330-0.html?utm_medium=ldc-partner&utm_source=SSPRC&utm_content=11878&utm_campaign=CD2146&cid=l0:en:pt:le:prosc:s88:330:ind:ssprc:CD2146&bid=11878&aid=CD2146\"><img src=\"http://lynda.directtrack.com/42/2146/11878/\" alt=\"Learn WordPress\" border=\"0\"></a>"
+    },
+
+
+
+    "mediaTemple" :{},
+
+    "maxcdn" :{}
+}
