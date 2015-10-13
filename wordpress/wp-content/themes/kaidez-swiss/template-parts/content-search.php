36db203c350186ee6c15e4186dd444280fbd0486
Kai Gittens <kai.gittens@gmail.com>
2015-10-13 06:45:17 -0400

    add/change search page elements

--- wordpress/wp-content/themes/kaidez-swiss/template-parts/content-search.php
+++ wordpress/wp-content/themes/kaidez-swiss/template-parts/content-search.php
@@ -11,21 +11,18 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
+		<?php the_title( sprintf( '<h1 class="entry-title search-result-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
 
 		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
+		<div class="search-entry-meta">
 			<?php kaidez_swiss_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php endif; ?>
 	</header><!-- .entry-header -->
 
-	<div class="entry-summary">
+	<div class="search-entry-summary">
 		<?php the_excerpt(); ?>
 	</div><!-- .entry-summary -->
 
-	<footer class="entry-footer">
-		<?php kaidez_swiss_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
 
