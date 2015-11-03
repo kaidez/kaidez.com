7a01daeecdf30b9084bfa17ff759e4c4a227f11b
Kai Gittens <kai.gittens@gmail.com>
2015-11-03 04:34:52 -0500

    fix whitespace

--- wordpress/webpack/entry_HOMEPAGE.js
+++ wordpress/webpack/entry_HOMEPAGE.js
@@ -1,11 +1,11 @@
-1/**
+/**
  * entry_HOMEPAGE.js
  *
  * Module for loading styles and scripts related to the home page only
  *
  */
 
- "use strict"; 
+"use strict"; 
 
 var $ = require( "jquery" ); // require jQuery
 
