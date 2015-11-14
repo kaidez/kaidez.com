07fe436a7d8be906f306b26886b30c896c235f2d
Kai Gittens <kai.gittens@gmail.com>
2015-11-14 08:33:59 -0500

    add Google Analytics code

--- wordpress/wp-content/themes/kaidez-swiss/footer-search.php
+++ wordpress/wp-content/themes/kaidez-swiss/footer-search.php
@@ -23,5 +23,14 @@
 <script src="/wp-content/themes/kaidez-swiss/js/common.js"></script>
 <script src="/wp-content/themes/kaidez-swiss/js/categoriesSearch.js"></script>
 
+<!-- Start Google Analytics -->
+ <script>
+    (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+    function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+    e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+    e.src='https://www.google-analytics.com/analytics.js';
+    r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+    ga('create','UA-5549541-1','auto');ga('send','pageview');
+</script>
 </body>
 </html>
