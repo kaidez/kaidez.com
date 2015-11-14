3a6beb8c51b71743bee3b873bb6efeec2fe80e7c
Kai Gittens <kai.gittens@gmail.com>
2015-11-14 16:44:01 -0500

    globally remove the "no-js" class

--- wordpress/wp-content/themes/kaidez-swiss/header-404.php
+++ wordpress/wp-content/themes/kaidez-swiss/header-404.php
@@ -8,7 +8,7 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?>>
 <head>
   <meta charset="UTF-8">
   <meta name=viewport content="width=device-width, initial-scale=1">
