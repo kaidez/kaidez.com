3fd28701e46782ebf24052f6b4b896ceab72de10
Kai Gittens <kai.gittens@gmail.com>
2016-07-15 19:38:28 -0400

    update second tweet example

--- samples/click-to-tweet/01/tweetButton.js
+++ samples/click-to-tweet/01/tweetButton.js
@@ -1,14 +1,11 @@
 var getPostTitle = document.getElementById("blog-post-title").innerHTML,
-  linkElement = document.getElementById("tweet-this-post"),
-  getPostLink = window.location.href;
-
- linkElement.setAttribute( "href", getPostLink );
+  linkElement = document.getElementById("tweet-this-post");
 
 $(linkElement).click(function(event){
 
   event.preventDefault();
 
-  var tweetedLink = this.getAttribute("href");
+  var tweetedLink = window.location.href;
 
   window.open("http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0");
 
