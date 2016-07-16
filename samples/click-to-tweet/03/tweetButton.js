faf63193438ae65ebb54b2eb1dc06a698b475a14
Kai Gittens <kai.gittens@gmail.com>
2016-07-15 20:00:59 -0400

    update code on 02 and 03 samples

--- samples/click-to-tweet/03/tweetButton.js
+++ samples/click-to-tweet/03/tweetButton.js
@@ -1,20 +1,13 @@
-(function(){
+var getPostTitle = document.getElementById("blog-post-title").innerHTML,
+  linkElement = document.getElementById("tweet-this-post");
 
-  var linkElement = document.getElementById( "tweet-this-post" ),
-      getPostTitle = document.getElementById( "blog-post-title" ).innerHTML,
-      getPostLink = window.location.origin + window.location.pathname,
-      cleanLink = getPostLink.replace( /[^/]*$/g, "" );
+$(linkElement).click(function(event){
 
-  linkElement.setAttribute( "href", cleanLink );
+  event.preventDefault();
 
-  $( linkElement ).on( "click", function( event ){
+  var tweetedLink = window.location.protocol + "//" + window.location.host + window.location.path;
+...
 
-    event.preventDefault();
+  window.open("http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0");
 
-    var tweetedLink = this.getAttribute( "href" );
-
-    window.open( "http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0" );
-
-  });
-
-})();
+});
