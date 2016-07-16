7c7616d0202bc9ba945724606cba521ee089d24b
Kai Gittens <kai.gittens@gmail.com>
2016-07-16 17:22:49 -0400

    start 04 sample folder for click to tweet post

---
+++ samples/click-to-tweet/04/tweetButton.js
@@ -0,0 +1,18 @@
+var getPostTitle = document.getElementById("blog-post-title").innerHTML,
+  linkElement = document.getElementById("tweet-this-post");
+
+$(linkElement).click(function(event){
+
+  event.preventDefault();
+
+  var tweetedLink;
+
+  if(!window.location.origin) {
+  	tweetedLink = window.location.protocol + "//" + window.location.host + window.location.pathname;
+  } else {
+  	tweetedLink = window.location.origin + window.location.pathname;
+  }
+
+  window.open("http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0");
+
+});
