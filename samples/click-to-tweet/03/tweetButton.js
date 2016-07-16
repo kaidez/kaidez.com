6ba9fe73146c66bca5cdcf4f04d8a0a0176df57c
Kai Gittens <kai.gittens@gmail.com>
2016-07-15 20:23:29 -0400

    remove typo

--- samples/click-to-tweet/03/tweetButton.js
+++ samples/click-to-tweet/03/tweetButton.js
@@ -6,7 +6,6 @@ $(linkElement).click(function(event){
   event.preventDefault();
 
   var tweetedLink = window.location.protocol + "//" + window.location.host + window.location.path;
-...
 
   window.open("http://twitter.com/intent/tweet?url=" + tweetedLink + "&text=" + getPostTitle + "&via=kaidez&", "twitterwindow", "height=450, width=550, toolbar=0, location=0, menubar=0, directories=0, scrollbars=0");
 
